3e627cdd1e22bebd88b808b016c466a9629a0e4a
Cory Martin <cory.r.martin@noaa.gov>
2022-03-25 15:19:33 -0400

    Add unit tests for YAML generation and verification (#25)
    * First attempt to test yaml generation
    
    * Start to valid yaml checker
    
    * Attempt to install fixed versions of solo and r2d2
    
    * Fix the tests so they fail unless the passed badge exists

---
+++ test/check_valid_yaml.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+import argparse
+import logging
+import os
+import pathlib
+import sys
+import yaml
+
+
+def check_valid_yaml(repodir):
+    logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s', level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
+    logging.info(f'Validating YAML files in {repodir}')
+    # get recursive list of YAML files to check
+    all_yamls = []
+    for path in pathlib.Path(os.path.join(repodir, 'test')).rglob('*.yaml'):
+        all_yamls.append(os.path.abspath(path))
+    for path in pathlib.Path(os.path.join(repodir, 'parm')).rglob('*.yaml'):
+        all_yamls.append(os.path.abspath(path))
+    for path in pathlib.Path(os.path.join(repodir, 'test')).rglob('*.yml'):
+        all_yamls.append(os.path.abspath(path))
+    for path in pathlib.Path(os.path.join(repodir, 'parm')).rglob('*.yml'):
+        all_yamls.append(os.path.abspath(path))
+    nfailed = 0
+    for yamlfile in all_yamls:
+        logging.info(f'Checking {yamlfile}')
+        try:
+            with open(yamlfile, 'r') as YAML_opened:
+                test_dict = yaml.safe_load(YAML_opened)
+        except Exception as e:
+            logging.error(f'Error occurred when attempting to load: {yamlfile}, error: {e}')
+            nfailed += 1
+    logging.info(f'{nfailed} of {len(all_yamls)} files failed.')
+    if nfailed > 0:
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('repodir', type=str, help='Path to repository root')
+    args = parser.parse_args()
+    check_valid_yaml(args.repodir)
