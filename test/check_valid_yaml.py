387f5038220d37cb92585811288ce5fd4cb6a6b0
Cory Martin <cory.r.martin@noaa.gov>
2022-03-25 12:36:41 -0400

    Commit for initial test

--- test/check_valid_yaml.py
+++ test/check_valid_yaml.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python3
 import argparse
-import glob
 import logger
+import pathlib
 import yaml
 
 
 def check_valid_yaml(repodir):
     logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s', level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
     logging.info(f'Validating YAML files in {repodir}')
+    # get recursive list of YAML files to check
+    all_yamls = []
+    for path in pathlib.Path(os.path.join(repodir, 'test')).rglob('*.yaml'):
+        all_yamls.append(path.name)
+    for path in pathlib.Path(os.path.join(repodir, 'parm')).rglob('*.yaml'):
+        all_yamls.append(path.name)
+    for path in pathlib.Path(os.path.join(repodir, 'test')).rglob('*.yml'):
+        all_yamls.append(path.name)
+    for path in pathlib.Path(os.path.join(repodir, 'parm')).rglob('*.yml'):
+        all_yamls.append(path.name)
+    for yamlfile in all_yamls:
+        print(yamlfile)
 
 
 if __name__ == "__main__":
