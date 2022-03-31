a7a535c21c5417be84f7ac9220e336d7fe6fd856
Cory Martin <cory.r.martin@noaa.gov>
2022-03-31 13:59:14 -0500

    Save progress

--- ush/run_satbias_conv.py
+++ ush/run_satbias_conv.py
@@ -16,6 +16,71 @@ logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s',
 def run_satbias_conv(config):
     # run ioda-converters satbias converter to take GSI satbias file
     # and create UFO compatible input files
+    # get times from config
+    startTime = config['start time']
+    endTime = config['end time']
+    assim_freq = int(config['assim_freq'])
+    # get paths from config
+    gsi_bc_root = config['gsi_bc_root']
+    ufo_bc_root = config['ufo_bc_root']
+    work_root = config['work_root']
+    converter_exe = config['satbias2ioda']
+    cdump = config.get('dump', 'gdas')
+
+    # loop through all cycles
+    nowTime = startTime
+    while nowTime <= endTime:
+        cdate = nowTime.strftime("%Y%m%d%H")
+        pdy = nowTime.strftime("%Y%m%d")
+        cyc = nowTime.strftime("%H")
+        logging.info(f'Processing sat bias files for cycle: {cdate}')
+        # create working directory
+        workdir = os.path.join(work_root, f'{cdump}.{pdy}', cyc, 'atmos')
+        os.makedirs(workdir, exist_ok=True)
+        # link the GSI files to the working directory
+        cycle_in_dir = os.path.join(gsi_bc_root, f'{cdump}.{pdy}', cyc, 'atmos')
+        prefix = f"{cdump}.t{cyc}z"
+        abias_path = os.path.join(cycle_in_dir, prefix + '.abias')
+        abias_pc_path = os.path.join(cycle_in_dir, prefix + '.abias_pc')
+        orig_paths = [converter_exe, abias_path, abias_pc_path]
+        new_paths = [
+            os.path.join(workdir, 'satbias2ioda.x'),
+            os.path.join(workdir, 'satbias_crtm_in'),
+            os.path.join(workdir, 'satbias_crtm_pc'),
+            ]
+        for src, dest in zip(orig_paths, new_paths):
+            if os.path.exists(dest):
+                os.unlink(dest)
+            os.symlink(src, dest)
+        # open the text file to get a list of satellites/sensors to process
+        satlist = []
+        print(new_paths[1])
+        with open(new_paths[1]) as csvfile:
+            reader = csv.reader(csvfile)
+            for row in reader:
+                splitrow = row[0].split()
+                if splitrow[1] not in satlist:
+                    try:
+                        a = float(splitrow[1])
+                    except ValueError:
+                        if len(splitrow[1]) > 0:
+                            satlist.append(splitrow[1])
+        # loop through satellites/sensors to write tlapmean txt file
+        for sat in satlist:
+            outstr = ''
+            outfile = os.path.join(workdir, f'{sat}_tlapmean.txt')
+            with open(new_paths[1]) as csvfile:
+                reader = csv.reader(csvfile)
+                for row in reader:
+                    splitrow = row[0].split()
+                    if splitrow[1] == sat:
+                        outstr = outstr + f'{sat} {splitrow[2]} {splitrow[3]}\n'
+            with open(outfile, 'w') as f:
+                f.write(outstr)
+
+
+        # advance to the next cycle
+        nowTime = nowTime + dt.timedelta(hours=assim_freq)
 
 
 if __name__ == "__main__":
