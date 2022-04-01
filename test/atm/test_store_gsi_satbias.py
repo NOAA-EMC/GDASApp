5286278696d7e55656bde307451187625a0409a7
Cory Martin <cory.r.martin@noaa.gov>
2022-04-01 13:58:40 -0500

    incremental save

---
+++ test/atm/test_store_gsi_satbias.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import os
+from r2d2 import store
+from solo.configuration import Configuration
+from solo.date import date_sequence
+import yaml
+
+if __name__ == "__main__":
+    # create a dummy R2D2 config YAML
+    r2d2_config = {
+        'databases': {'test': {'cache_fetch': False,
+                               'class': 'LocalDB',
+                               'root': os.path.join(os.getcwd(), 'r2d2-test')},
+                     },
+        'fetch_order': ['test'],
+        'store_order': ['test'],
+    }
+    with open(os.path.join(os.getcwd(), 'testinput', 'r2d2_config_test.yaml'), 'w') as f:
+        yaml.dump(r2d2_config, f,  sort_keys=False, default_flow_style=False)
+    os.environ['R2D2_CONFIG'] = os.path.join(os.getcwd(), 'testinput', 'r2d2_config_test.yaml')
