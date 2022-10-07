b361bc8e9487a0da2782ecedf23b4358c6ec36ee
Cory Martin <cory.r.martin@noaa.gov>
2022-10-07 17:43:22 +0000

    Use spack-stack and switch to tags/commit hashes for JEDI repos (#146)
    * Check out commit hashes for most repos
    
    * Use spack-stack for orion and hera
    
    * Have build script not complain about PS1
    
    * Update BUFR2IODA test YAML
    
    * Add engine syntax to yaml
    
    * Slight change to paths and do not copy BC files for conventional
    
    * Add gsibec (optional) to build (#142)
    
    * fix gen_Eva_obs_yaml
    
    * Marine DA bugfix (#149)
    
    * removed soca run step from tests (#150)
    
    Co-authored-by: Andrew Collard <andrew.collard@noaa.gov>
    Co-authored-by: RussTreadon-NOAA <russ.treadon@noaa.gov>
    Co-authored-by: Guillaume Vernieres <guillaume.vernieres@noaa.gov>

--- test/soca/static.sh
+++ test/soca/static.sh
@@ -9,9 +9,11 @@ mkdir -p ${soca_static}
 mkdir -p ${soca_static}/INPUT
 mkdir -p ${soca_static}/bump
 
-lowres=${project_source_dir}/soca/test/Data/72x35x25
-cp -L ${lowres}/{diag_table,field_table,MOM_input} ${soca_static}
-cp -L ${lowres}/../{fields_metadata.yml,godas_sst_bgerr.nc,rossrad.dat} ${soca_static}
+lowres=${project_source_dir}/soca/test/Data
+
+cp -L ${lowres}/workdir/{diag_table,field_table} ${soca_static}
+cp -L ${project_source_dir}/test/soca/fix/MOM_input ${soca_static}
+cp -L ${lowres}/{fields_metadata.yml,godas_sst_bgerr.nc,rossrad.dat} ${soca_static}
 mv ${soca_static}/fields_metadata.yml ${soca_static}/fields_metadata.yaml
-cp -L ${lowres}/input.nml ${soca_static}/inputnml
-cp -L ${lowres}/INPUT/{hycom1_25.nc,ocean_mosaic.nc,grid_spec.nc,layer_coord25.nc,ocean_hgrid.nc,ocean_topog.nc} ${soca_static}/INPUT
+cp -L ${lowres}/72x35x25/input.nml ${soca_static}/inputnml
+cp -L ${lowres}/72x35x25/INPUT/{hycom1_25.nc,ocean_mosaic.nc,grid_spec.nc,layer_coord25.nc,ocean_hgrid.nc,ocean_topog.nc} ${soca_static}/INPUT
