71a685d377b4443984642ea79f733c5c1bd68137
russ.treadon <Russ.Treadon@noaa.gov>
2022-10-28 18:11:03 +0000

    configure hera and orion 3dvar yaml to use gsibec, add identity staticb to stage.py (#166)

--- ush/ufsda/stage.py
+++ ush/ufsda/stage.py
@@ -36,12 +36,15 @@ def gdas_fix(input_fix_dir, working_dir, config):
     # figure out staticb source
     if config['STATICB_TYPE'] == 'bump':
         staticb_source = 'bump'
+    elif config['STATICB_TYPE'] == 'identity':
+        staticb_source = 'identity'
     else:
         staticb_source = 'gsibec'
 
     # link staticb
-    ufsda.disk_utils.symlink(os.path.join(input_fix_dir, staticb_source, case_anl),
-                             config['fv3jedi_staticb_dir'])
+    if staticb_source in ['bump', 'gsibec']:
+        ufsda.disk_utils.symlink(os.path.join(input_fix_dir, staticb_source, case_anl),
+                                 config['fv3jedi_staticb_dir'])
 
     # link akbk file
     ufsda.disk_utils.symlink(os.path.join(input_fix_dir, 'fv3jedi',
