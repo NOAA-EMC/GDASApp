7258b5b15b0f13987ab0eb1e4feafea6e4aab444
Jiarui Dong <Jiarui.Dong@noaa.gov>
2022-11-29 12:19:35 -0500

    Remove local module load.

--- test/land/create_bkg_ens.sh
+++ test/land/create_bkg_ens.sh
@@ -13,10 +13,7 @@ FILEDATE=$YYYY$MM$DD.${HH}0000
 project_binary_dir=$1
 project_source_dir=$2
 
-module purge
-module use $project_source_dir/modulefiles
-module load GDAS/hera
-
+WORKDIR=$project_binary_dir/test/testrun
 RSTDIR=$GDASAPP_TESTDATA/lowres/gdas.$YYYY$MM$DD/$HR/atmos/RESTART/
 DAtype=letkfoi_snow
 
@@ -29,21 +26,21 @@ if [[ ${DAtype} == 'letkfoi_snow' ]]; then
     # FOR LETKFOI, CREATE THE PSEUDO-ENSEMBLE
     for ens in 001 002
     do
-        if [ -e $project_binary_dir/test/testrun/mem${ens} ]; then
-                rm -r $project_binary_dir/test/testrun/mem${ens}
+        if [ -e $WORKDIR/mem${ens} ]; then
+                rm -r $WORKDIR/mem${ens}
         fi
-        mkdir $project_binary_dir/test/testrun/mem${ens}
+        mkdir $WORKDIR/mem${ens}
         for tile in 1 2 3 4 5 6
         do
-        cp ${RSTDIR}/${FILEDATE}.sfc_data.tile${tile}.nc  ${project_binary_dir}/test/testrun/mem${ens}/${FILEDATE}.sfc_data.tile${tile}.nc
+        cp ${RSTDIR}/${FILEDATE}.sfc_data.tile${tile}.nc  ${WORKDIR}/mem${ens}/${FILEDATE}.sfc_data.tile${tile}.nc
         done
-        cp ${RSTDIR}/${FILEDATE}.coupler.res ${project_binary_dir}/test/testrun/mem${ens}/${FILEDATE}.coupler.res
+        cp ${RSTDIR}/${FILEDATE}.coupler.res ${WORKDIR}/mem${ens}/${FILEDATE}.coupler.res
     done
 
 
     echo 'do_landDA: calling create ensemble'
 
-    python ${project_source_dir}/ush/land/letkf_create_ens.py $FILEDATE $B $project_binary_dir/test/testrun
+    python ${project_source_dir}/ush/land/letkf_create_ens.py $FILEDATE $B $WORKDIR
     if [[ $? != 0 ]]; then
         echo "letkf create failed"
         exit 10
