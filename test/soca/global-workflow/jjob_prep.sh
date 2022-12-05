0342aa0afb747ff85b13e137990a61c72447e874
Guillaume Vernieres <guillaume.vernieres@noaa.gov>
2022-12-05 17:20:22 -0500

    blank ctests for jjobs

---
+++ test/soca/global-workflow/jjob_prep.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+bindir=$1
+srcdir=$2
+
+# source configs
+
+# prepare stuff from previous cycle
+
+# run jjob
---
+++ test/soca/global-workflow/setup_workflow_exp.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# ctest to create an experiment directory for global-workflow
+bindir=$1
+srcdir=$2
+
+# test experiment variables
+idate=2021032318
+edate=2021032418
+app=ATM # NOTE make this S2SWA soon
+starttype='warm'
+gfscyc='4'
+resdet='48'
+resens='24'
+nens=0
+pslot='gdas_test'
+configdir=$srcdir/../../parm/config
+comrot=$bindir/test/soca/global-workflow/testrun/ROTDIRS
+expdir=$bindir/test/soca/global-workflow/testrun/experiments
+
+# clean previous experiment
+rm -rf $comrot $expdir
+
+# run the script
+ln -sf $srcdir/../../workflow/setup_expt.py .
+
+echo "Running global-workflow experiment generation script"
+./setup_expt.py cycled --idate $idate  \
+                       --edate $edate \
+                       --app $app \
+                       --start $starttype \
+                       --gfs_cyc $gfscyc \
+                       --resdet $resdet \
+                       --resens $resens \
+                       --nens $nens \
+                       --pslot $pslot \
+                       --configdir $configdir \
+                       --comrot $comrot \
+                       --expdir $expdir
+
+exit $?
