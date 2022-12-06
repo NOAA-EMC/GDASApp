c66b37c3c5921c9191ced654053d8a7fd1f0946e
Cory Martin <cory.r.martin@noaa.gov>
2022-12-06 12:22:42 -0600

    switch to using pygw for yaml check

--- test/check_valid_yaml.py
+++ test/check_valid_yaml.py
@@ -3,9 +3,8 @@ import argparse
 import logging
 import os
 import pathlib
-import re
 import sys
-import yaml
+from pygw.yaml_file import YAMLFile
 
 
 def check_valid_yaml(repodir):
@@ -22,60 +21,11 @@ def check_valid_yaml(repodir):
     for path in pathlib.Path(os.path.join(repodir, 'parm')).rglob('*.yml'):
         all_yamls.append(os.path.abspath(path))
     nfailed = 0
-    # define tags
-    envtag = '!ENV'
-    inctag = '!INC'
-    # pattern for global vars: look for ${word}
-    pattern = re.compile(r'.*?\${(\w+)}.*?')
-    loader = yaml.SafeLoader
-
-    # the envtag will be used to mark where to start searching for the pattern
-    # e.g. somekey: !ENV somestring${MYENVVAR}blah blah blah
-    loader.add_implicit_resolver(envtag, pattern, None)
-    loader.add_implicit_resolver(inctag, pattern, None)
-
-    def expand_env_variables(line):
-        match = pattern.findall(line)  # to find all env variables in line
-        if match:
-            full_value = line
-            for g in match:
-                full_value = full_value.replace(
-                    f'${{{g}}}', os.environ.get(g, f'${{{g}}}')
-                )
-            return full_value
-        return line
-
-    def constructor_env_variables(loader, node):
-        """
-        Extracts the environment variable from the node's value
-        :param yaml.Loader loader: the yaml loader
-        :param node: the current node in the yaml
-        :return: the parsed string that contains the value of the environment
-        variable
-        """
-        value = loader.construct_scalar(node)
-        return expand_env_variables(value)
-
-    def constructor_include_variables(loader, node):
-        """
-        Extracts the environment variable from the node's value
-        :param yaml.Loader loader: the yaml loader
-        :param node: the current node in the yaml
-        :return: the content of the file to be included
-        """
-        value = loader.construct_scalar(node)
-        value = expand_env_variables(value)
-        expanded = parse_yaml(value)
-        return expanded
-
-    loader.add_constructor(envtag, constructor_env_variables)
-    loader.add_constructor(inctag, constructor_include_variables)
 
     for yamlfile in all_yamls:
         logging.info(f'Checking {yamlfile}')
         try:
-            with open(yamlfile, 'r') as YAML_opened:
-                test_dict = yaml.load(YAML_opened, Loader=loader)
+            config = YAMLFile(path=yamlfile)
         except Exception as e:
             logging.error(f'Error occurred when attempting to load: {yamlfile}, error: {e}')
             nfailed += 1
