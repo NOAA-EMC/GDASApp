cfaa0a045b43692ba8b932b83426bad2a222bb7f
Cory Martin <cory.r.martin@noaa.gov>
2022-12-16 21:55:10 +0000

    Make genYAML not depend on ufsda module; allows for UFO acceptance testing to work again (#249)

--- test/genYAML_prep.sh
+++ test/genYAML_prep.sh
@@ -8,23 +8,12 @@ output: ${bindir}/test/testoutput/genYAML_output_3dvar.yaml
 config:
   BERROR_YAML: \${PARMgfs}/atm/berror/staticb_bump.yaml
   OBS_YAML_DIR: \${PARMgfs}/atm/obs/config
-  atm: true
+  COMPONENT: atmos
   layout_x: '1'
   layout_y: '2'
-  BKG_DIR: /this/is/not/a/real/path
   OBS_LIST: ${srcdir}/parm/atm/obs/lists/gdas_prototype.yaml
-  fv3jedi_fix_dir: Data/fv3files
-  fv3jedi_fieldset_dir: Data/fieldsets
-  ANL_DIR: /fake/path/to/analysis
-  fv3jedi_staticb_dir: /fake/path/to/berror
-  BIAS_DIR: /fake/path/to/biascoeff
-  CRTM_COEFF_DIR: /fake/path/to/crtm
-  BIAS_PREFIX: gdas.t18z.
-  BIAS_DATE: '2022033000'
-  DIAG_DIR: /fake/output/path
-  OBS_DIR: /fake/path/to/obs
-  OBS_PREFIX: gdas.t00z.
-  OBS_DATE: '2022033000'
+  OPREFIX: gdas.t00z.
+  CDATE: '2022033000'
   valid_time: '2022-03-30T00:00:00Z'
   atm_window_length: PT6H
 EOF
