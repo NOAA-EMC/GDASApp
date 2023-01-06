328a7cb8fe3c50ca6d6888e46847b17eeda79fb5
AndrewEichmann-NOAA <58948505+AndrewEichmann-NOAA@users.noreply.github.com>
2023-01-06 12:09:37 -0500

    adding more error handling to soca run scripts, other refinements (#262)

--- test/soca/gw/jjob_prep.sh
+++ test/soca/gw/jjob_prep.sh
@@ -2,12 +2,17 @@
 bindir=$1
 srcdir=$2
 
+
 # export env. var.
 source "${srcdir}/test/soca/gw/runtime_vars.sh" "${bindir}" "${srcdir}"
 
+OCNDIR="${ROTDIR}/${PSLOT}/gdas.${PDY}/${gcyc}/ocean/" 
+
+rm -r ${OCNDIR}
+
 # prepare background from previous cycle
-mkdir -p "${ROTDIR}/${PSLOT}/gdas.${PDY}/${gcyc}/ocean/"
-cp -r "${bindir}/test/soca/bkg/"* "${ROTDIR}/${PSLOT}/gdas.${PDY}/${gcyc}/ocean/"
+mkdir -p ${OCNDIR}
+cp -r "${bindir}/test/soca/bkg/"* ${OCNDIR}
 
 # run prep jjob
 "${HOMEgfs}/jobs/JGDAS_GLOBAL_OCEAN_ANALYSIS_PREP"
