34e61164142d09ff1c981592d8474fd14759ede8
Jiarui Dong <Jiarui.Dong@noaa.gov>
2023-01-06 15:46:17 -0500

    Upgrade a 'create_ens' script to remove water and glacier locations. (#259)
    * Upgrade a 'create_ens' script to remove water and glacier locations.
    
    * Fix python coding norm errors.
    
    * Fix python coding norm issues.
    
    * Fix python coding norm issue.
    
    * Update to use the latest commit hashes for imsproc and jediincr.

--- ush/land/letkf_create_ens.py
+++ ush/land/letkf_create_ens.py
@@ -31,6 +31,9 @@ for (mem, value) in zip(ens_dirs, sign):
         # open file
         out_netcdf = os.path.join(workdir, mem, f"{fstub}.sfc_data.tile{tt}.nc")
         with Dataset(out_netcdf, "r+") as ncOut:
+            slmsk_array = ncOut.variables['slmsk'][:]
+            vtype_array = ncOut.variables['vtype'][:]
+            slmsk_array[vtype_array == 15] = 0  # remove glacier locations
             var_array = ncOut.variables[vname][:]
-            var_array = var_array + value*offset
+            var_array[slmsk_array == 1] = var_array[slmsk_array == 1] + value*offset
             ncOut.variables[vname][0, :, :] = var_array[:]
