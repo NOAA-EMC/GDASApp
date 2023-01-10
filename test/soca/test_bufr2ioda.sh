e5c691d94dab0abe0a99a79d95a36281da63f527
AndrewEichmann-NOAA <Andrew.Eichmann@noaa.gov>
2023-01-10 13:44:20 -0600

    remove output before db test, tweaks to bufr2ioda test

--- test/soca/test_bufr2ioda.sh
+++ test/soca/test_bufr2ioda.sh
@@ -4,11 +4,11 @@ set -x
 
 PROJECT_BINARY_DIR=${1}
 OBSSOURCE=${2}
-
-rm -fv ${PROJECT_BINARY_DIR}/test/testoutput/${OBSSOURCE}_201804*.nc
-
 CMD=${PROJECT_BINARY_DIR}/bin/bufr2ioda.x
 OBSYAML=${PROJECT_BINARY_DIR}/test/testinput/${OBSSOURCE}.yaml
+
+rm -fv ${PROJECT_BINARY_DIR}/test/testoutput/${OBSSOURCE}_20180415.nc
+rm -fv ${PROJECT_BINARY_DIR}/test/testoutput/${OBSSOURCE}_201804.nc
 
 ${CMD} ${OBSYAML}
 
