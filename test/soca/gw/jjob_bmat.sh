fc4631281e5038dd5cd9640582896941a614359a
AndrewEichmann-NOAA <andrew.eichmann@noaa.gov>
2023-01-13 17:22:08 +0000

    adding bmat jjobwq

---
+++ test/soca/gw/jjob_bmat.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+bindir=$1
+srcdir=$2
+
+# export env. var.
+source "${srcdir}/test/soca/gw/runtime_vars.sh" "${bindir}" "${srcdir}"
+
+# run DA jjob
+"${HOMEgfs}/jobs/JGDAS_GLOBAL_OCEAN_ANALYSIS_BMAT"
