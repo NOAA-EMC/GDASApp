f7c23af6f7389c36817825ff9714be27d7068491
RussTreadon-NOAA <26926959+RussTreadon-NOAA@users.noreply.github.com>
2023-01-24 08:12:50 -0500

    Enable run_jedi_exe.py to run 3dvar (#278)
    * Restore 3dvar capability to run_jedi_exe.py (#223)
    
    * Correct python norm errors (#223)
    
    * Remove yaml_template from 3dvar_orion.yaml (#223)
    
    * add genYAML.py; clean up run_jedi_exe.py (#223)
    
    * Update genYAML to use ufsda genYAML.py (#223)
    
    * add ufsda.genYAML to __init__.py (#223)
    
    * remove genYAML from yamltools.py (#223)
    
    * remove __main__ from genYAML.py
    
    * revert changes to ush/genYAML
    
    * remove stage from init, simplify genYAML
    
    * import specific ufsda.stage functions in run_jedi_exe.py
    
    * replace exclusive with mem=0 in sbatch jobcard
    
    * add ufsda.stage import to marine_analysis_prep script
    
    * use RussTreadon-NOAA/global-workflow feature/ufsda_stage in automated tests
    
    * Revert "use RussTreadon-NOAA/global-workflow feature/ufsda_stage in automated tests"
    
    This reverts commit 3c828e2695b4e5eaa00dc77a8b56999c14c83089.
    
    * update keywords in hera 3dvar yaml
    
    * provide default for NMEM_ENKF in run_jedi_exe.py

--- ush/ufsda/__init__.py
+++ ush/ufsda/__init__.py
@@ -1,8 +1,8 @@
 from .disk_utils import mkdir, symlink
 from .ufs_yaml import gen_yaml, parse_config
-import ufsda.stage
 import ufsda.archive
 import ufsda.r2d2
 import ufsda.post
 import ufsda.yamltools
+import ufsda.genYAML
 from .misc_utils import isTrue, create_batch_job, submit_batch_job
