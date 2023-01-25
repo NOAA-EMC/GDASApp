724f05f9c3520f22751840fb0e16bc96f66ae9e3
Guillaume Vernieres <guillaume.vernieres@noaa.gov>
2023-01-25 13:03:43 -0500

    a few more plots

--- scripts/exgdas_global_marine_analysis_vrfy.py
+++ scripts/exgdas_global_marine_analysis_vrfy.py
@@ -44,9 +44,10 @@ import matplotlib.pyplot as plt
 import ufsda
 
 
-def plot_config(grid_file, data_file, variable, levels, bounds, colormap, comout):
+def plot_config(grid_file=[], data_file=[],
+                variable=[], levels=[], bounds=[], colormap=[], comout=[], lats=[]):
     """
-    Prepares the configuration for the plotting function below
+    Prepares the configuration for the plotting functions below
     """
     config = {}
     config['grid file'] = grid_file
@@ -55,7 +56,9 @@ def plot_config(grid_file, data_file, variable, levels, bounds, colormap, comout
     config['levels'] = levels
     config['bounds'] = bounds
     config['colormap'] = colormap
+    config['lats'] = lats
     config['comout'] = comout
+    config['max depth'] = 5000.0
     return config
 
 
@@ -67,14 +70,14 @@ def plot_horizontal_slice(config):
     grid = xr.open_dataset(config['grid file'])
     data = xr.open_dataset(config['fields file'])
     if config['variable'] in ['Temp', 'Salt', 'u', 'v']:
-        slice_data = data[config['variable']].sel(Layer=level)
+        slice_data = np.squeeze(data[config['variable']])[level, :, :]
     else:
-        slice_data = data[config['variable']]
+        slice_data = np.squeeze(data[config['variable']])
     bounds = config['bounds']
     fig, ax = plt.subplots(figsize=(8, 5), subplot_kw={'projection': ccrs.PlateCarree()})
     plt.pcolormesh(np.squeeze(grid.lon),
                    np.squeeze(grid.lat),
-                   np.squeeze(slice_data),
+                   slice_data,
                    vmin=bounds[0], vmax=bounds[1],
                    transform=ccrs.PlateCarree(),
                    cmap=config['colormap'])
@@ -82,30 +85,125 @@ def plot_horizontal_slice(config):
     ax.coastlines()
     ax.gridlines(draw_labels=True)
     ax.add_feature(cartopy.feature.LAND)
-    dirname = os.path.join(config['comout'], 'vrfy', config['variable'])
+    dirname = os.path.join(config['comout'], config['variable'])
     ufsda.mkdir(dirname)
     figname = os.path.join(dirname, config['variable']+'_Level_'+str(level))
     plt.savefig(figname, bbox_inches='tight')
 
 
+def plot_zonal_slice(config):
+    """
+    pcolormesh of a zonal slice of an ocean field
+    """
+    lat = float(config['lats'][0])
+    grid = xr.open_dataset(config['grid file'])
+    data = xr.open_dataset(config['fields file'])
+    lat_index = np.argmin(np.array(np.abs(np.squeeze(grid.lat)[:, 0]-lat)))
+    slice_data = np.squeeze(np.array(data[config['variable']].sel(yaxis_1=lat_index)))
+    depth = np.squeeze(np.array(grid['h'].sel(yaxis_1=lat_index)))
+    depth[np.where(np.abs(depth) > 10000.0)] = 0.0
+    depth = np.cumsum(depth, axis=0)
+    bounds = config['bounds']
+    x = np.tile(np.squeeze(grid.lon[:, lat_index]), (np.shape(depth)[0], 1))
+    fig, ax = plt.subplots(figsize=(8, 5))
+    plt.pcolormesh(x, -depth, slice_data,
+                   vmin=bounds[0], vmax=bounds[1],
+                   cmap=config['colormap'])
+    plt.colorbar(label=config['variable']+' Lat '+str(lat), shrink=0.5, orientation='horizontal')
+    ax.set_ylim(-config['max depth'], 0)
+    dirname = os.path.join(config['comout'], config['variable'])
+    ufsda.mkdir(dirname)
+    figname = os.path.join(dirname, config['variable'] +
+                           'zonal_lat_'+str(int(lat)) + '_' + str(int(config['max depth'])) + 'm')
+    plt.savefig(figname, bbox_inches='tight')
+
+
 comout = os.getenv('COMOUT')
+cyc = os.getenv('cyc')
+bcyc = str((int(cyc) - 3) % 24)
+
+grid_file = os.path.join(comout, 'gdas.t'+bcyc+'z.ocngrid.nc')
+
+#######################################
+# INCREMENT
+#######################################
+incr_cmap = 'RdBu'
+data_file = os.path.join(comout, 'gdas.t'+cyc+'z.ocninc.nc')
+config = plot_config(grid_file=grid_file,
+                     data_file=data_file,
+                     colormap=incr_cmap,
+                     comout=os.path.join(comout, 'vrfy', 'incr'))
+
+#######################################
+# zonal slices
+
+for lat in np.arange(-60, 60, 10):
+
+    for max_depth in [700.0, 5000.0]:
+        config['lats'] = [lat]
+        config['max depth'] = max_depth
+
+        # Temperature
+        config.update({'variable': 'Temp', 'levels': [1], 'bounds': [-.5, .5]})
+        plot_zonal_slice(config)
+
+        # Salinity
+        config.update({'variable': 'Salt', 'levels': [1], 'bounds': [-.1, .1]})
+        plot_zonal_slice(config)
+
+#######################################
+# Horizontal slices
 
 # Temperature
-config = plot_config(grid_file=os.path.join(comout, 'gdas.t9z.ocngrid.nc'),
-                     data_file=os.path.join(comout, 'gdas.t12z.ocninc.nc'),
-                     variable='Temp',
-                     levels=[1],
-                     bounds=[-1, 1],
-                     colormap='bwr',
-                     comout=comout)
+config.update({'variable': 'Temp', 'levels': [1], 'bounds': [-1, 1]})
 plot_horizontal_slice(config)
 
 # Salinity
-config['variable'] = 'Salt'
-config['bounds'] = [-0.1, 0.1]
+config.update({'variable': 'Salt', 'bounds': [-0.1, 0.1]})
 plot_horizontal_slice(config)
 
 # Sea surface height
-config['variable'] = 'ave_ssh'
-config['bounds'] = [-0.1, 0.1]
+config.update({'variable': 'ave_ssh', 'bounds': [-0.1, 0.1]})
+plot_horizontal_slice(config)
+
+#######################################
+# Std Bkg. Error
+#######################################
+bmat_cmap = 'hot'
+data_file = os.path.join(comout, 'gdas.t'+cyc+'z.ocn.bkgerr_stddev.nc')
+config = plot_config(grid_file=grid_file,
+                     data_file=data_file,
+                     colormap=bmat_cmap,
+                     comout=os.path.join(comout, 'vrfy', 'bkgerr'))
+
+#######################################
+# zonal slices
+
+for lat in np.arange(-60, 60, 10):
+
+    for max_depth in [700.0, 5000.0]:
+        config['lats'] = [lat]
+        config['max depth'] = max_depth
+
+        # Temperature
+        config.update({'variable': 'Temp', 'levels': [1], 'bounds': [0, 1.5]})
+        plot_zonal_slice(config)
+
+        # Salinity
+        config.update({'variable': 'Salt', 'levels': [1], 'bounds': [0, .2]})
+        plot_zonal_slice(config)
+
+#######################################
+# Horizontal slices
+
+# Temperature
+config.update({'variable': 'Temp', 'levels': [1], 'bounds': [0, 2]})
+plot_horizontal_slice(config)
+
+# Salinity
+config.update({'variable': 'Salt', 'bounds': [0, 0.2]})
+plot_horizontal_slice(config)
+
+# Sea surface height
+config.update({'variable': 'ave_ssh', 'bounds': [0, 0.1]})
 plot_horizontal_slice(config)
