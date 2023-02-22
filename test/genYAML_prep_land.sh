c3b15854b0afb49a8e57e7aae567f60b228b627b
Jiarui Dong <Jiarui.Dong@noaa.gov>
2023-02-22 13:32:37 -0500

    Modify ush/ufsda/genYAML.py to generate Yaml file for land DA.

---
+++ test/genYAML_prep_land.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+bindir=$1
+srcdir=$2
+#bindir=/scratch2/NCEPDEV/stmp1/Jiarui.Dong/gdasapp/gdasapp.6
+#srcdir=/scratch2/NCEPDEV/stmp1/Jiarui.Dong/gdasapp/gdasapp.6
+#PARMgfs=/scratch2/NCEPDEV/stmp1/Jiarui.Dong/gdasapp/gdasapp.6/parm
+
+cat > testoutput/genYAML_example_land.yaml << EOF
+template: ${srcdir}/parm/land/letkfoi/letkfoi.yaml
+output: ${bindir}/test/testoutput/genYAML_output_letkfoi.yaml
+config:
+  BERROR_YAML: \${PARMgfs}/atm/berror/staticb_bump.yaml
+  OBS_YAML_DIR: \${PARMgfs}/land/obs/config
+  COMPONENT: land
+  layout_x: '1'
+  layout_y: '2'
+  OBS_LIST: ${srcdir}/parm/land/obs/lists/gdas_land_prototype.yaml
+  OPREFIX: gdas.t18z.
+  CDATE: '2021032318'
+  PDY: '20210323'
+  cyc: '18'
+  valid_time: '2021-03-23T18:00:00Z'
+  atm_window_length: PT6H
+  land_window_length: PT6H
+  CASE: 'C48'
+  CASE_ENKF: 'C48'
+  LEVS: '128'
+EOF
