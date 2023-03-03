0afb1258624fd8099939baa7a8f2d5ed0f11832d
Andrew.Tangborn <Andrew.Tangborn@noaa.gov>
2023-03-03 18:59:59 +0000

    Test for builiding aero DA yaml.

---
+++ test/genYAML_prep_aero.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+bindir=$1
+srcdir=$2
+
+cat > testoutput/genYAML_example_aero.yaml << EOF
+template: ${srcdir}/parm/aero/variational/3dvar_gfs_aero.yaml
+output: ${bindir}/test/testoutput/genYAML_output_3dvar_gfs_aero.yaml
+config:
+  BERROR_YAML: \${PARMgfs}/aero/berror/staticb_bump.yaml
+  OBS_YAML_DIR: \${PARMgfs}/aero/obs/config
+  COMPONENT: aero 
+  layout_x: '8'
+  layout_y: '8'
+  OBS_LIST: ${srcdir}/parm/aero/obs/lists/gdas_aero_prototype.yaml
+  OPREFIX: gdas.t06z.
+  CDATE: '2019061400'
+  PDY: '20190614'
+  cyc: '06'
+  valid_time: '2019-06-14T06:00:00Z'
+  aero_window_length: PT6H
+  CASE: 'C48'
+  CASE_ENKF: 'C48'
+  LEVS: '128'
+EOF
--- ush/ufsda/genYAML.py
+++ ush/ufsda/genYAML.py
@@ -72,7 +72,7 @@ def get_runtime_config(config_dict):
     bkg_tstep = str(config_dict.get('BKG_TSTEP', f"PT{assim_freq}H"))
     component_dict = {
         'atmos': 'ATM',
-        'chem': 'AERO',
+        'aero': 'AERO',
         'ocean': 'SOCA',
         'land': 'LAND',
     }
@@ -90,6 +90,13 @@ def get_runtime_config(config_dict):
         fv3_begin_var = 'LAND_BEGIN_YYYYmmddHHMMSS'
         bkg_string_var = 'LAND_BKG_YYYYmmddHHMMSS'
         bkg_isotime_var = 'LAND_BKG_ISOTIME'
+    if 'aero_window_length' in config_dict.keys():
+        win_begin_var = component_dict[config_dict.get('COMPONENT', 'aero')] + '_WINDOW_BEGIN'
+        win_end_var = component_dict[config_dict.get('COMPONENT', 'aero')] + '_WINDOW_END'
+        win_len_var = component_dict[config_dict.get('COMPONENT', 'aero')] + '_WINDOW_LENGTH'
+        fv3_begin_var = 'AERO_BEGIN_YYYYmmddHHMMSS'
+        bkg_string_var = 'AERO_BKG_YYYYmmddHHMMSS'
+        bkg_isotime_var = 'AERO_BKG_ISOTIME'
     npx_ges_var = 'npx_ges'
     npy_ges_var = 'npy_ges'
     npz_ges_var = 'npz_ges'
