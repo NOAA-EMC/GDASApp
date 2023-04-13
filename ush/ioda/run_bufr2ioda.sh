00c617d85202656901efe3c9d6959c8c0970766a
CoryMartin-NOAA <cory.r.martin@noaa.gov>
2023-04-13 20:26:27 +0000

    Small changes to save

--- ush/ioda/run_bufr2ioda.sh
+++ ush/ioda/run_bufr2ioda.sh
@@ -16,11 +16,11 @@ if [[ $# -ne 5 ]] ; then
 fi
 
 # input parameters
-CDATE=$1
-RUN=$2
-BUFR_dir=$3
-YAML_template_dir=$4
-out_dir=$5
+CDATE=${CDATE:-$1}
+RUN=${RUN:-$2}
+BUFR_dir=${BUFR_dir:-$3}
+YAML_template_dir=${YAML_template_dir:-$4}
+out_dir=${out_dir:-$5}
 
 # derived parameters
 PDY=$(echo $CDATE | cut -c1-8)
@@ -48,21 +48,24 @@ for f in $BUFR_files; do
     BUFRtype=$(echo "${BUFRtype_base#*.*.}")
     echo "Now processing $BUFRtype"
     # get path to YAML template file for this BUFR file
-    YAML_template=$BUFR_dir/bufr_${BUFRtype}.yaml
+    YAML_template=$YAML_template_dir/bufr_${BUFRtype}.yaml
     # check if the YAML template exists
     if [ ! -f $YAML_template ]; then
         echo "${YAML_template} does not exist, skipping ${BUFRtype}!"
         continue
     fi
     # need to create a usable YAML from the template
+    
+
+    # run BUFR2IODA for the created YAML file
 
 done
 
 # do extra stuff for the prepbufr file
 #-----------
 # first, check if the prepBUFR file exists
-if [ -f $BUFR_dir/${RUN}.t${cyc}z.prepbufr ]; then
-    echo "Will process $BUFR_dir/${RUN}.t${cyc}z.prepbufr"
-    # there are multiple YAMLs to run through
-    prepBUFR_YAMLS="prepbufr_adpupa prepbufr_adpsfc"
-fi
+#if [ -f $BUFR_dir/${RUN}.t${cyc}z.prepbufr ]; then
+#    echo "Will process $BUFR_dir/${RUN}.t${cyc}z.prepbufr"
+#    # there are multiple YAMLs to run through
+#    prepBUFR_YAMLS="prepbufr_adpupa prepbufr_adpsfc"
+#fi
