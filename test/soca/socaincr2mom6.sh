39eb8b53c82696ce8bb438d6e9be8eb95f4f2b8c
Guillaume Vernieres <guillaume.vernieres@noaa.gov>
2023-04-25 17:33:43 -0400

    fixed data src for soca 2 mom6 test

--- test/soca/socaincr2mom6.sh
+++ test/soca/socaincr2mom6.sh
@@ -3,7 +3,6 @@ set -ex
 
 srcdir=$1
 
-data="/home/gvernier/wcda-sprint/wrk"
 mom6_iau_incr="gdas.t12z.ocn.incr.nc"
 
 cat > nsst.yaml << EOF
@@ -11,7 +10,6 @@ sfc_fcst: ${data}/gdas.t06z.sfcf006.nc
 sfc_ana: ${data}/gdas.t12z.sfcanl.nc
 nlayers: 2
 EOF
-
 
 ${srcdir}/ush/socaincr2mom6.py --incr "${data}/ocn.3dvarfgat_pseudo.incr.2018-04-15T09:00:00Z.nc" \
                                --bkg "${data}/gdas.t06z.ocnf006.nc" \
