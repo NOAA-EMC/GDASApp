73a661b8b9095d4e2eb9d1df99f246d6b4c36da8
Cory Martin <cory.r.martin@noaa.gov>
2023-08-07 17:05:36 -0400

    A few more hopefully helpful comments

--- utils/ioda_example/gdas_meanioda.cc
+++ utils/ioda_example/gdas_meanioda.cc
@@ -4,6 +4,8 @@
 // this is an example application that
 // will use IODA to read a file and print something
 // it is intended to be very bare bones
+// you will note the .cc file is very empty
+// the .h file is where the action is!
 
 int main(int argc, char ** argv) {
   oops::Run run(argc, argv);
