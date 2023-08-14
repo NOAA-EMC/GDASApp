fd2a710a77ab0ad8dd914188b31cdec2f5caed6c
CoryMartin-NOAA <cory.r.martin@noaa.gov>
2023-08-14 16:57:51 +0000

    start to refactor run script

--- ush/ioda/run_bufr2ioda.sh
+++ ush/ioda/run_bufr2ioda.sh
@@ -2,25 +2,24 @@
 #--------------------------------------------------------------------------------------
 # run_bufr2ioda.sh
 # This driver script will:
-# - determine list of input BUFR files available
-# - generate YAMLs from templates for each BUFR file
-# - run BUFR2IODA.x and produce output IODA files
+# - generate config files from templates for each BUFR file
+# - use bufr2ioda python scripts or executable and produce output IODA files
 # usage:
-#       run_bufr2ioda.sh YYYYMMDDHH /path/to/files.bufr_d/ /path/to/templates.yaml/ /path/to/output.ioda/
+#       run_bufr2ioda.sh YYYYMMDDHH $DMPDIR /path/to/templates $COM_OBS
 #
 #--------------------------------------------------------------------------------------
 if [[ $# -ne 5 ]] ; then
     echo "usage:"
-    echo "      $0 YYYYMMDDHH gdas|gfs /path/to/files.bufr_d/ /path/to/templates.yaml/ /path/to/output.ioda/"
+    echo "      $0 YYYYMMDDHH gdas|gfs /path/to/files.bufr_d/ /path/to/templates /path/to/output.ioda/"
     exit 1
 fi
 
 # input parameters
 CDATE=${CDATE:-$1}
 RUN=${RUN:-$2}
-BUFR_dir=${BUFR_dir:-$3}
-YAML_template_dir=${YAML_template_dir:-$4}
-out_dir=${out_dir:-$5}
+DMPDIR=${DMPDIR:-$3}
+config_template_dir=${config_template_dir:-$4}
+COM_OBS=${COM_OBS:-$5}
 
 # derived parameters
 PDY=$(echo $CDATE | cut -c1-8)
@@ -29,75 +28,46 @@ cyc=$(echo $CDATE | cut -c9-10)
 # get gdasapp root directory
 readonly DIR_ROOT=$(cd "$(dirname "$(readlink -f -n "${BASH_SOURCE[0]}" )" )/../.." && pwd -P)
 BUFR2IODA=$DIR_ROOT/build/bin/bufr2ioda.x
-BUFRYAMLGEN=$DIR_ROOT/ush/ioda/gen_bufr2ioda_yaml.py
-
-# get list of BUFR files in input directory using glob
-BUFR_files=$(ls $BUFR_dir/${RUN}.t${cyc}z.*.bufr_d)
-if [ $? -ne 0 ]; then
-    echo "No BUFR files found! in $BUFR_dir"
-    exit 1
-fi
+USH_IODA=$DIR_ROOT/ush/ioda
+BUFRYAMLGEN=$USH_IODA/gen_bufr2ioda_yaml.py
+BUFRJSONGEN=$USH_IODA/gen_bufr2ioda_json.py
 
 # create output directory if it doesn't exist
-mkdir -p $out_dir
+mkdir -p $COM_OBS
 if [ $? -ne 0 ]; then
-    echo "cannot make $out_dir"
+    echo "cannot make $COM_OBS"
     exit 1
 fi
 
-# loop through available BUFR files
-for f in $BUFR_files; do
-    # get BUFR type from input BUFR file name
-    BUFRbase=$(basename $f)
-    BUFRtype_base=$(echo "${BUFRbase%.*.*}")
-    BUFRtype=$(echo "${BUFRtype_base#*.*.}")
-    echo "Now processing $BUFRtype"
-    # get path to YAML template file for this BUFR file
-    YAML_template=$YAML_template_dir/bufr_${BUFRtype}.yaml
-    # check if the YAML template exists
-    if [ ! -f $YAML_template ]; then
-        echo "${YAML_template} does not exist, skipping ${BUFRtype}!"
-        continue
-    fi
+#---------------------------------------------------------
+# for some BUFR files, we will use python scripts
+# and for others, bufr2ioda.x
 
-    # input YAML file to the template parser
-    cat > $out_dir/config_bufr_${BUFRtype}.yaml << EOF
-obtype: $BUFRtype
-input file: $f
-output yaml file: $out_dir/bufr_${BUFRtype}.yaml
-output dir: $out_dir
-template yaml: $YAML_template
-run: $RUN
-PDY: $PDY
-cyc: $cyc
-EOF
+#---------------------------
+#----- python and json -----
+# first specify what observation types will be processed by a script
+BUFR_py="satwind_amv_goes"
 
-    # now create YAML from the template
-    $BUFRYAMLGEN --config $out_dir/config_bufr_${BUFRtype}.yaml
+for obtype in $BUFR_py; do
+  # this loop assumes that there is a python script and template with the same name
+  echo "Processing ${obtype}..."
 
-    # run BUFR2IODA for the created YAML file
-    $BUFR2IODA $out_dir/bufr_${BUFRtype}.yaml
+  # first generate a JSON from the template
+  ${BUFRJSONGEN} -t ${config_template_dir}/bufr2ioda_${obtype}.json -o ${COM_OBS}/${obtype}_${PDY}${cyc}.json
 
-    # check if converter was successful
-    if [ $? == 0 ]; then
-      # remove YAMLs if success
-      rm -rf $out_dir/config_bufr_${BUFRtype}.yaml
-      rm -rf $out_dir/bufr_${BUFRtype}.yaml
-    else
-      # warn and keep YAMLs on failure
-      echo "Problem running bufr2ioda.x for ${BUFRtype}"
-      echo "See YAMLs in $out_dir"
-      echo "  $out_dir/config_bufr_${BUFRtype}.yaml"
-      echo "  $out_dir/bufr_${BUFRtype}.yaml"
-    fi
+  # now use the converter script for the ob type
 
+  # check if converter was successful
+  if [ $? == 0 ]; then
+    # remove JSON file
+    rm -rf ${COM_OBS}/${obtype}_${PDY}${cyc}.json
+  else
+    # warn and keep the JSON file
+    echo "Problem running converter script for ${obtype}"
+  fi
 done
 
-# do extra stuff for the prepbufr file
-#-----------
-# first, check if the prepBUFR file exists
-#if [ -f $BUFR_dir/${RUN}.t${cyc}z.prepbufr ]; then
-#    echo "Will process $BUFR_dir/${RUN}.t${cyc}z.prepbufr"
-#    # there are multiple YAMLs to run through
-#    prepBUFR_YAMLS="prepbufr_adpupa prepbufr_adpsfc"
-#fi
+#----------------------------
+#---- bufr2ioda and yaml ----
+# need to ask Emily which will be YAML based
+BUFR_yaml=""
