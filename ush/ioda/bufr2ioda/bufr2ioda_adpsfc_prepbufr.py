4aae2831823b498d9333451f4c7d3f4c269be59d
Nicholas Esposito <esposito@Orion-login-2.HPC.MsState.Edu>
2023-09-12 18:15:45 -0400

    finally works again with json

--- ush/ioda/bufr2ioda/bufr2ioda_adpsfc_prepbufr.py
+++ ush/ioda/bufr2ioda/bufr2ioda_adpsfc_prepbufr.py
@@ -3,6 +3,8 @@
 # This software is licensed under the terms of the Apache Licence Version 2.0
 # which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
 
+#!/usr/bin/env python3
+
 import sys
 import numpy as np
 import os
