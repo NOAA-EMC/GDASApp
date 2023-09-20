10e18869b629431e51c93b18052c361d58a7629b
AndrewEichmann-NOAA <andrew.eichmann@noaa.gov>
2023-09-20 21:50:21 +0000

    change prints to logs

--- scripts/exglobal_prep_ocean_obs.py
+++ scripts/exglobal_prep_ocean_obs.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
 # exglobal_prep_ocean_obs.py
-# Pepares observations for coupled ocean DA
+# Pepares observations for marine DA
 import os
 from wxflow import YAMLFile
 import prep_marine_obs
+import logging
 
 OBS_YAML = os.getenv('OBS_YAML')
-print("OBS_YAML:", OBS_YAML)
 
 data = YAMLFile(OBS_YAML)
 print(data)
 
 for observer in data['observers']:
     obs_source_name = observer['obs space']['name']
-    print(obs_source_name)
+    dwprint(obs_source_name)
+    logging.info(f"obs_source_name: {obs_source_name}")
     prep_marine_obs.obs_fetch(obs_source_name)
