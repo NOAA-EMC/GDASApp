b82a83136039ce3fee57ec5090272bb688c185e9
AndrewEichmann-NOAA <andrew.eichmann@noaa.gov>
2023-09-20 20:12:49 +0000

    ripped out fancy OO version, now plain vanilla

--- scripts/exglobal_prep_ocean_obs.py
+++ scripts/exglobal_prep_ocean_obs.py
@@ -9,21 +9,9 @@ OBS_YAML = os.getenv('OBS_YAML')
 print("OBS_YAML:", OBS_YAML)
 
 data = YAMLFile(OBS_YAML)
-
-# The following loop tries to find classes in prep_marine_obs corresponding
-# to the observer names (traditionally of the form sensor_platform) with "_obs"
-# appended, intantiate them, and then call their respective methods
+print(data)
 
 for observer in data['observers']:
     obs_source_name = observer['obs space']['name']
     print(obs_source_name)
-    obs_source_name = obs_source_name + '_obs'
-    try:
-        obs_source = getattr(prep_marine_obs, obs_source_name)
-    except AttributeError:
-        print("WARNING: No class", obs_source_name, "in prep_marine_obs")
-        continue
-    obs_set = obs_source()
-    obs_set.fetch()
-    obs_set.convert()
-    obs_set.concatenate()
+    prep_marine_obs.obs_fetch(obs_source_name)
--- ush/soca/prep_marine_obs.py
+++ ush/soca/prep_marine_obs.py
@@ -6,273 +6,77 @@ import os
 import fnmatch
 
 
-class ocean_observation:
 
-    DMPDIR = os.getenv('DMPDIR')
-    cyc = os.getenv('cyc')
-    PDY = os.getenv('PDY')
-    CDUMP = os.getenv('CDUMP')
-    COMIN_OBS = os.getenv('COMIN_OBS')
+DMPDIR = os.getenv('DMPDIR')
+cyc = os.getenv('cyc')
+PDY = os.getenv('PDY')
+CDUMP = os.getenv('CDUMP')
+COMIN_OBS = os.getenv('COMIN_OBS')
 
-# Variables of convenience - raided from scripts/exgdas_global_marine_analysis_prep.py
-    half_assim_freq = timedelta(hours=int(os.getenv('assim_freq'))/2)
-    window_length = timedelta(hours=int(os.getenv('assim_freq')))
-    window_middle = datetime.strptime(PDY+cyc, '%Y%m%d%H')
-    window_begin = datetime.strptime(PDY+cyc, '%Y%m%d%H') - half_assim_freq
-    window_end = datetime.strptime(PDY+cyc, '%Y%m%d%H') + half_assim_freq
-    window_begin_iso = window_begin.strftime('%Y-%m-%dT%H:%M:%SZ')
-    window_middle_iso = window_middle.strftime('%Y-%m-%dT%H:%M:%SZ')
-    fcst_begin = datetime.strptime(PDY+cyc, '%Y%m%d%H')
+cycdir = os.path.join( DMPDIR, CDUMP + '.' + str(PDY), str(cyc))
 
-    def __init__(self):
+obs_dict = {
+    #                   'rads_adt_3a_2021182.nc'
+    'adt_3a_egm2008': ('ADT', 'rads_adt_3a_???????.nc'),
 
-        self.cycdir = os.path.join(
-            self.DMPDIR, self.CDUMP + '.' + str(self.PDY), str(self.cyc))
+    #                   'rads_adt_3b_2021182.nc'
+    'adt_3b_egm2008': ('ADT', 'rads_adt_3b_???????.nc'),
 
-    def fetch(self, subdir, filepattern):
+    #                   'rads_adt_c2_2021182.nc'
+    'adt_c2_egm2008': ('ADT', 'rads_adt_c2_???????.nc'),
 
-        datadir = os.path.join(self.cycdir, subdir)
-        # TODO: check the existence of this
-        print('datadir:', datadir)
-        matching_files = []
+    #                   'rads_adt_j2_2021182.nc'
+    'adt_j2': ('ADT', 'rads_adt_j2_???????.nc'),
 
-        for root, _, files in os.walk(datadir):
-            for filename in fnmatch.filter(files, filepattern):
-                matching_files.append((root, filename))
+    #                   'rads_adt_j3_2021182.nc'
+    'adt_j3': ('ADT', 'rads_adt_j3_???????.nc'),
 
-        obs_cpy = []
-        for obs_src in matching_files:
-            obs_path = os.path.join(obs_src[0], obs_src[1])
-            obs_dst = os.path.join(self.COMIN_OBS, obs_src[1])
-            obs_cpy.append([obs_path, obs_dst])
+    #                   'rads_adt_sa_2021182.nc'
+    'adt_sa_egm2008': ('ADT', 'rads_adt_sa_???????.nc'),
 
-        print(obs_cpy)
+    #       'AMSR2-SEAICE-NH_v2r2_GW1_s202107011426180_e202107011605170_c202107011642250.nc'
+    'icec_amsr2_north': ('icec', 'AMSR2-SEAICE-NH_v2r2_GW1_s???????????????_e???????????????_c???????????????.nc'),
 
-        FileHandler({'copy': obs_cpy}).sync()
+    #                   'AMSR2-SEAICE-SH_v2r2_GW1_s202107011426180_e202107011605170_c202107011642250.nc'
+    'icec_amsr2_south': ('icec', 'AMSR2-SEAICE-SH_v2r2_GW1_s???????????????_e???????????????_c???????????????.nc'),
 
-    def convert(self):
-        # Call ioda converter
-        pass
+    #                   'SMAP_L2B_SSS_NRT_34268_A_20210701T153914.h5'
+    'sss_smap': ('SSS', 'SMAP_L2B_SSS_NRT_?????_[AD]_????????T??????.h5'),
 
-    def concatenate(self):
-        pass
+    #                     '20210701150000-OSPO-L3U_GHRSST-SSTsubskin-VIIRS_N20-ACSPO_V2.61-v02.0-fv01.0.nc'
+    'sst_viirs_n20_l3u_so025': ('sst', '??????????????-OSPO-L3U_GHRSST-SSTsubskin-VIIRS_N20-ACSPO_V2.61-v02.0-fv01.0.nc'),
 
+}
 
-class adt_3a_egm2008_obs(ocean_observation):
 
-    def __init__(self):
-        super().__init__()
+def obs_fetch(obs_source_name):
 
-    def fetch(self):
+    try:
+        obs_source = obs_dict[obs_source_name]
+    except KeyError:
+        print(f'WARNING: no obs source {obs_source_name} defined, skipping')
+        return
 
-        subdir = 'ADT'
-#                   'rads_adt_3a_2021182.nc'
-        filepattern = 'rads_adt_3a_???????.nc'
+    subdir = obs_source[0]
+    filepattern = obs_source[1]  
 
-        super().fetch(subdir, filepattern)
+    datadir = os.path.join(cycdir, subdir)
+    # TODO: check the existence of this
+    print('datadir:', datadir)
+    matching_files = []
 
-    def convert(self):
-        # Call ioda converter
-        pass
+    for root, _, files in os.walk(datadir):
+        for filename in fnmatch.filter(files, filepattern):
+            matching_files.append((root, filename))
 
-    def concatenate(self):
+    obs_cpy = []
+    for obs_src in matching_files:
+        obs_path = os.path.join(obs_src[0], obs_src[1])
+        obs_dst = os.path.join(COMIN_OBS, obs_src[1])
+        obs_cpy.append([obs_path, obs_dst])
 
-        pass
+    print(obs_cpy)
 
+    FileHandler({'copy': obs_cpy}).sync()
 
-class adt_3b_egm2008_obs(ocean_observation):
 
-    def __init__(self):
-        super().__init__()
-
-    def fetch(self):
-
-        subdir = 'ADT'
-#                   'rads_adt_3b_2021182.nc'
-        filepattern = 'rads_adt_3b_???????.nc'
-
-        super().fetch(subdir, filepattern)
-
-    def convert(self):
-        # Call ioda converter
-        pass
-
-    def concatenate(self):
-
-        pass
-
-
-class adt_c2_egm2008_obs(ocean_observation):
-
-    def __init__(self):
-        super().__init__()
-
-    def fetch(self):
-
-        subdir = 'ADT'
-#                   'rads_adt_c2_2021182.nc'
-        filepattern = 'rads_adt_c2_???????.nc'
-
-        super().fetch(subdir, filepattern)
-
-    def convert(self):
-        # Call ioda converter
-        pass
-
-    def concatenate(self):
-
-        pass
-
-
-class adt_j2_obs(ocean_observation):
-
-    def __init__(self):
-        super().__init__()
-
-    def fetch(self):
-
-        subdir = 'ADT'
-#                   'rads_adt_j2_2021182.nc'
-        filepattern = 'rads_adt_j2_???????.nc'
-
-        super().fetch(subdir, filepattern)
-
-    def convert(self):
-        # Call ioda converter
-        pass
-
-    def concatenate(self):
-
-        pass
-
-
-class adt_j3_obs(ocean_observation):
-
-    def __init__(self):
-        super().__init__()
-
-    def fetch(self):
-
-        subdir = 'ADT'
-#                   'rads_adt_j3_2021182.nc'
-        filepattern = 'rads_adt_j3_???????.nc'
-
-        super().fetch(subdir, filepattern)
-
-    def convert(self):
-        # Call ioda converter
-        pass
-
-    def concatenate(self):
-
-        pass
-
-
-class adt_sa_egm2008_obs(ocean_observation):
-
-    def __init__(self):
-        super().__init__()
-
-    def fetch(self):
-
-        subdir = 'ADT'
-#                   'rads_adt_sa_2021182.nc'
-        filepattern = 'rads_adt_sa_???????.nc'
-
-        super().fetch(subdir, filepattern)
-
-    def convert(self):
-        # Call ioda converter
-        pass
-
-    def concatenate(self):
-
-        pass
-
-
-class icec_amsr2_north_obs(ocean_observation):
-
-    def __init__(self):
-        super().__init__()
-
-    def fetch(self):
-
-        subdir = 'icec'
-#       'AMSR2-SEAICE-NH_v2r2_GW1_s202107011426180_e202107011605170_c202107011642250.nc'
-        filepattern = 'AMSR2-SEAICE-NH_v2r2_GW1_s???????????????_e???????????????_c???????????????.nc'
-
-        super().fetch(subdir, filepattern)
-
-    def convert(self):
-        # Call ioda converter
-        pass
-
-    def concatenate(self):
-
-        pass
-
-
-class icec_amsr2_south_obs(ocean_observation):
-
-    def __init__(self):
-        super().__init__()
-
-    def fetch(self):
-
-        subdir = 'icec'
-#                   'AMSR2-SEAICE-SH_v2r2_GW1_s202107011426180_e202107011605170_c202107011642250.nc'
-        filepattern = 'AMSR2-SEAICE-SH_v2r2_GW1_s???????????????_e???????????????_c???????????????.nc'
-
-        super().fetch(subdir, filepattern)
-
-    def convert(self):
-        # Call ioda converter
-        pass
-
-    def concatenate(self):
-
-        pass
-
-
-class sss_smap_obs(ocean_observation):
-
-    def __init__(self):
-        super().__init__()
-
-    def fetch(self):
-
-        subdir = 'SSS'
-#                   'SMAP_L2B_SSS_NRT_34268_A_20210701T153914.h5'
-        filepattern = 'SMAP_L2B_SSS_NRT_?????_[AD]_????????T??????.h5'
-
-        super().fetch(subdir, filepattern)
-
-    def convert(self):
-        # Call ioda converter
-        pass
-
-    def concatenate(self):
-
-        pass
-
-
-class sst_viirs_n20_l3u_so025(ocean_observation):
-
-    def __init__(self):
-        super().__init__()
-
-    def fetch(self):
-
-        subdir = 'sst'
-#                     '20210701150000-OSPO-L3U_GHRSST-SSTsubskin-VIIRS_N20-ACSPO_V2.61-v02.0-fv01.0.nc'
-        filepattern = '??????????????-OSPO-L3U_GHRSST-SSTsubskin-VIIRS_N20-ACSPO_V2.61-v02.0-fv01.0.nc'
-
-        super().fetch(subdir, filepattern)
-
-    def convert(self):
-        # Call ioda converter
-        pass
-
-    def concatenate(self):
-
-        pass
