3706fe73bd8416f03bee04f3442aa3e14b2d2202
Shastri Paturi <shastri.paturi@noaa.gov>
2023-09-25 19:41:35 +0000

    added GHRSST SST MetOp-B files

--- utils/test/prepdata.sh
+++ utils/test/prepdata.sh
@@ -4,3 +4,5 @@ set -e
 project_source_dir=$1
 
 ncgen -o rads_adt_j3_2021182.nc4 ${project_source_dir}/testdata/rads_adt_j3_2021182.cdl
+ncgen -o ghrsst_sst_mb_202107010000.cdl ${project_source_dir}/testdata/ghrsst_sst_mb_202107010000.cdl
+ncgen -o ghrsst_sst_mb_202107010100.cdl ${project_source_dir}/testdata/ghrsst_sst_mb_202107010100.cdl
