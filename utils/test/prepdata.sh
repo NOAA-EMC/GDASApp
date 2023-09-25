2ff854e42e0af9ffcb06c710233c23f31319c0c9
AndrewEichmann-NOAA <58948505+AndrewEichmann-NOAA@users.noreply.github.com>
2023-09-25 14:38:58 -0400

    adding icec amsr2 data prep (#639)

--- utils/test/prepdata.sh
+++ utils/test/prepdata.sh
@@ -4,3 +4,7 @@ set -e
 project_source_dir=$1
 
 ncgen -o rads_adt_j3_2021182.nc4 ${project_source_dir}/testdata/rads_adt_j3_2021182.cdl
+ncgen -o icec_amsr2_north_1.nc4 ${project_source_dir}/testdata/icec_amsr2_north_1.cdl
+ncgen -o icec_amsr2_north_2.nc4 ${project_source_dir}/testdata/icec_amsr2_north_2.cdl
+ncgen -o icec_amsr2_south_1.nc4 ${project_source_dir}/testdata/icec_amsr2_south_1.cdl
+ncgen -o icec_amsr2_south_2.nc4 ${project_source_dir}/testdata/icec_amsr2_south_2.cdl
