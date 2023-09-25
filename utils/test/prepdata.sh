0c47472936170362e8484c8ce82774b0853aa94a
AndrewEichmann-NOAA <andrew.eichmann@noaa.gov>
2023-09-25 18:59:44 +0000

    added sss obs to test

--- utils/test/prepdata.sh
+++ utils/test/prepdata.sh
@@ -8,3 +8,7 @@ ncgen -o icec_amsr2_north_1.nc4 ${project_source_dir}/testdata/icec_amsr2_north_
 ncgen -o icec_amsr2_north_2.nc4 ${project_source_dir}/testdata/icec_amsr2_north_2.cdl
 ncgen -o icec_amsr2_south_1.nc4 ${project_source_dir}/testdata/icec_amsr2_south_1.cdl
 ncgen -o icec_amsr2_south_2.nc4 ${project_source_dir}/testdata/icec_amsr2_south_2.cdl
+ncgen -o sss_smap_1.nc4 ${project_source_dir}/testdata/sss_smap_1.cdl
+ncgen -o sss_smap_2.nc4 ${project_source_dir}/testdata/sss_smap_2.cdl
+ncgen -o sss_smos_1.nc4 ${project_source_dir}/testdata/sss_smos_1.cdl
+ncgen -o sss_smos_1.nc4 ${project_source_dir}/testdata/sss_smos_2.cdl
