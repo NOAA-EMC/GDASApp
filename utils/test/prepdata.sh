653c5c7f0e8c93a1fded707bacb14e3de3100531
Mindo Choi <mindo.choi@noaa.gov>
2023-09-25 18:15:19 +0000

    adding lists of data and preparation script

--- utils/test/prepdata.sh
+++ utils/test/prepdata.sh
@@ -13,3 +13,4 @@ ncgen -o rads_adt_j3_2021181.nc4 ${project_source_dir}/testdata/rads_adt_j3_2021
 ncgen -o rads_adt_j3_2021182.nc4 ${project_source_dir}/testdata/rads_adt_j3_2021182.cdl
 ncgen -o rads_adt_sa_2021181.nc4 ${project_source_dir}/testdata/rads_adt_sa_2021181.cdl
 ncgen -o rads_adt_sa_2021182.nc4 ${project_source_dir}/testdata/rads_adt_sa_2021182.cdl
+
