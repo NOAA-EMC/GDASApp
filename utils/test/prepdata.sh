c6616fec9b232ae5946f378d3da32bd507045f92
Mindo Choi <mindo.choi@noaa.gov>
2023-09-25 19:20:41 +0000

    fixed conflict file merging from team members

--- utils/test/prepdata.sh
+++ utils/test/prepdata.sh
@@ -3,7 +3,16 @@ set -e
 
 project_source_dir=$1
 
+ncgen -o rads_adt_3a_2021181.nc4 ${project_source_dir}/testdata/rads_adt_3a_2021181.cdl
+ncgen -o rads_adt_3b_2021181.nc4 ${project_source_dir}/testdata/rads_adt_3b_2021181.cdl
+ncgen -o rads_adt_3a_2021182.nc4 ${project_source_dir}/testdata/rads_adt_3a_2021182.cdl
+ncgen -o rads_adt_3b_2021182.nc4 ${project_source_dir}/testdata/rads_adt_3b_2021182.cdl
+ncgen -o rads_adt_c2_2021181.nc4 ${project_source_dir}/testdata/rads_adt_c2_2021181.cdl
+ncgen -o rads_adt_c2_2021182.nc4 ${project_source_dir}/testdata/rads_adt_c2_2021182.cdl
+ncgen -o rads_adt_j3_2021181.nc4 ${project_source_dir}/testdata/rads_adt_j3_2021181.cdl
 ncgen -o rads_adt_j3_2021182.nc4 ${project_source_dir}/testdata/rads_adt_j3_2021182.cdl
+ncgen -o rads_adt_sa_2021181.nc4 ${project_source_dir}/testdata/rads_adt_sa_2021181.cdl
+ncgen -o rads_adt_sa_2021182.nc4 ${project_source_dir}/testdata/rads_adt_sa_2021182.cdl
 ncgen -o icec_amsr2_north_1.nc4 ${project_source_dir}/testdata/icec_amsr2_north_1.cdl
 ncgen -o icec_amsr2_north_2.nc4 ${project_source_dir}/testdata/icec_amsr2_north_2.cdl
 ncgen -o icec_amsr2_south_1.nc4 ${project_source_dir}/testdata/icec_amsr2_south_1.cdl
