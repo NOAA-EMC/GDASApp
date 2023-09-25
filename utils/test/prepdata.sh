27996451aeef6ccc1ec3bf0a08ac45d4cf0b8af4
Mindo Choi <141867620+apchoiCMD@users.noreply.github.com>
2023-09-25 17:03:05 -0400

    adt data sets are added for obs prep (#641)
    * fixed conflict file merging from team members
    
    * Update preparation of data for Rad obs
    
    * unnecessary testdata files deleted
    
    ---------
    
    Co-authored-by: Guillaume Vernieres <guillaume.vernieres@noaa.gov>

--- utils/test/prepdata.sh
+++ utils/test/prepdata.sh
@@ -3,7 +3,8 @@ set -e
 
 project_source_dir=$1
 
-ncgen -o rads_adt_j3_2021182.nc4 ${project_source_dir}/testdata/rads_adt_j3_2021182.cdl
+ncgen -o rads_adt_3a_2021181.nc4 ${project_source_dir}/testdata/rads_adt_3a_2021181.cdl
+ncgen -o rads_adt_3b_2021181.nc4 ${project_source_dir}/testdata/rads_adt_3b_2021181.cdl
 ncgen -o icec_amsr2_north_1.nc4 ${project_source_dir}/testdata/icec_amsr2_north_1.cdl
 ncgen -o icec_amsr2_north_2.nc4 ${project_source_dir}/testdata/icec_amsr2_north_2.cdl
 ncgen -o icec_amsr2_south_1.nc4 ${project_source_dir}/testdata/icec_amsr2_south_1.cdl
