efac95c6f81838494a24e9563cdf6cc23595fa88
Guillaume Vernieres <guillaume.vernieres@noaa.gov>
2023-11-28 11:13:26 -0500

    Add an option to point to the "enkfgdas" backgrounds instead of the pre-generated 30 member ensemble (#771)
    #### Work done 
    Mainly what the title says. The default ctest is now assuming that
    `DOHYBVAR`=`YES`, I don't really think it's worth it to cover the
    `DOHYBVAR`=`NO` in the ctest. I may change my mind, but in both cases,
    `soca` is configured to use the hybrid envar solver, the only difference
    is the origin of the ensemble members.
    
    #### Work left to be done
    We need to define an ensemble "COM_OCEAN/ICE_HISTORY_PREV" environment
    variable in the prep jjob (in the g-w). Flagged as a `TODO` in the code.

--- scripts/exgdas_global_marine_analysis_prep.py
+++ scripts/exgdas_global_marine_analysis_prep.py
@@ -89,6 +89,10 @@ def cice_hist2fms(input_filename, output_filename):
     # open the CICE history file
     ds = xr.open_dataset(input_filename)
 
+    if 'aicen' in ds.variables and 'hicen' in ds.variables and 'hsnon' in ds.variables:
+        logging.info(f"*** Already reformatted, skipping.")
+        return
+
     # rename the dimensions to xaxis_1 and yaxis_1
     ds = ds.rename({'ni': 'xaxis_1', 'nj': 'yaxis_1'})
 
@@ -224,6 +228,11 @@ logging.info(f"---------------- Setup runtime environement")
 comin_obs = os.getenv('COMIN_OBS')  # R2D2 DB for now
 anl_dir = os.getenv('DATA')
 staticsoca_dir = os.getenv('SOCA_INPUT_FIX_DIR')
+if os.getenv('DOHYBVAR') == "YES":
+    dohybvar = True
+    nmem_ens = int(os.getenv('NMEM_ENS'))
+else:
+    dohybvar = False
 
 # create analysis directories
 diags = os.path.join(anl_dir, 'diags')            # output dir for soca DA obs space
@@ -242,6 +251,7 @@ window_middle_iso = window_middle.strftime('%Y-%m-%dT%H:%M:%SZ')
 fcst_begin = datetime.strptime(os.getenv('PDY')+os.getenv('cyc'), '%Y%m%d%H')
 RUN = os.getenv('RUN')
 cyc = os.getenv('cyc')
+gcyc = os.getenv('gcyc')
 PDY = os.getenv('PDY')
 
 ################################################################################
@@ -310,21 +320,43 @@ for domain in ['ocn', 'ice']:
 FileHandler({'copy': bkgerr_list}).sync()
 
 ################################################################################
-# stage static ensemble
+# stage ensemble members
+if dohybvar:
+    logging.info("---------------- Stage ensemble members")
+    nmem_ens = int(os.getenv('NMEM_ENS'))
+    longname = {'ocn': 'ocean', 'ice': 'ice'}
+    ens_member_list = []
+    for mem in range(1, nmem_ens+1):
+        for domain in ['ocn', 'ice']:
+            # TODO(Guillaume): make use and define ensemble COM in the j-job
+            ensdir = os.path.join(os.getenv('COM_OCEAN_HISTORY_PREV'), '..', '..', '..', '..', '..',
+                                  f'enkf{RUN}.{PDY}', f'{gcyc}', f'mem{str(mem).zfill(3)}',
+                                  'model_data', longname[domain], 'history')
+            ensdir = os.path.normpath(ensdir)
+            f009 = f'enkfgdas.t{gcyc}z.{domain}f009.nc'
 
-logging.info(f"---------------- Stage climatological ensemble")
-clim_ens_member_list = []
-clim_ens_dir = find_clim_ens(pytz.utc.localize(window_begin, is_dst=None))
-clim_ens_size = len(glob.glob(os.path.abspath(os.path.join(clim_ens_dir, 'ocn.*.nc'))))
-os.environ['CLIM_ENS_SIZE'] = str(clim_ens_size)
+            fname_in = os.path.abspath(os.path.join(ensdir, f009))
+            fname_out = os.path.abspath(os.path.join(static_ens, domain+"."+str(mem)+".nc"))
+            ens_member_list.append([fname_in, fname_out])
+    FileHandler({'copy': ens_member_list}).sync()
 
-for domain in ['ocn', 'ice']:
-    for mem in range(1, clim_ens_size+1):
-        fname = domain+"."+str(mem)+".nc"
-        fname_in = os.path.abspath(os.path.join(clim_ens_dir, fname))
-        fname_out = os.path.abspath(os.path.join(static_ens, fname))
-        clim_ens_member_list.append([fname_in, fname_out])
-FileHandler({'copy': clim_ens_member_list}).sync()
+    # reformat the cice history output
+    for mem in range(1, nmem_ens+1):
+        cice_fname = os.path.abspath(os.path.join(static_ens, "ice."+str(mem)+".nc"))
+        cice_hist2fms(cice_fname, cice_fname)
+else:
+    logging.info("---------------- Stage offline ensemble members")
+    ens_member_list = []
+    clim_ens_dir = find_clim_ens(pytz.utc.localize(window_begin, is_dst=None))
+    nmem_ens = len(glob.glob(os.path.abspath(os.path.join(clim_ens_dir, 'ocn.*.nc'))))
+    for domain in ['ocn', 'ice']:
+        for mem in range(1, nmem_ens+1):
+            fname = domain+"."+str(mem)+".nc"
+            fname_in = os.path.abspath(os.path.join(clim_ens_dir, fname))
+            fname_out = os.path.abspath(os.path.join(static_ens, fname))
+            ens_member_list.append([fname_in, fname_out])
+    FileHandler({'copy': ens_member_list}).sync()
+os.environ['ENS_SIZE'] = str(nmem_ens)
 
 ################################################################################
 # prepare JEDI yamls
@@ -456,7 +488,6 @@ if 'SABER_BLOCKS_YAML' in os.environ and os.environ['SABER_BLOCKS_YAML']:
 else:
     logging.info(f"using default SABER blocks yaml")
     os.environ['SABER_BLOCKS_YAML'] = os.path.join(gdas_home, 'parm', 'soca', 'berror', 'saber_blocks.yaml')
-os.environ['CLIM_ENS_SIZE'] = str(clim_ens_size)
 
 # substitute templated variables in the var config
 logging.info(f"{config}")
