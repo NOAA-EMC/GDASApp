624d2a69947922bda2ca5cb166502cd059e96f13
YoulongXia-NOAA <Youlong.Xia@noaa.gov>
2024-01-11 11:06:39 -0600

    Change 2023 into 2024 based on reviewer's comment

--- ush/ioda/bufr2ioda/bufr2ioda_snocvr_bufr.py
+++ ush/ioda/bufr2ioda/bufr2ioda_snocvr_bufr.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# (C) Copyright 2023 NOAA/NWS/NCEP/EMC
+# (C) Copyright 2024 NOAA/NWS/NCEP/EMC
 #
 # This software is licensed under the terms of the Apache Licence Version 2.0
 # which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
