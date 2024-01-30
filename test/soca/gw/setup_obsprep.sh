d11ae9b439b60fb1720fd5662850c87c488e2780
ShastriPaturi <shastri.paturi@noaa.gov>
2024-01-30 14:19:43 -0600

    copy monthly in situ BUFR to DMPDIR

--- test/soca/gw/setup_obsprep.sh
+++ test/soca/gw/setup_obsprep.sh
@@ -11,8 +11,9 @@ fi
 
 project_source_dir="$1"
 testdatadir="${project_source_dir}/test/soca/testdata"
+testdatadir_bufr="${project_source_dir}/build/gdas/test/testdata"
 
-#clean up previous attempts
+# Clean up previous attempts
 rm -rf gdas.20180414  gdas.20180415
 
 # Define PDYs, cycs, and obstypes
@@ -20,30 +21,34 @@ PDYs=("20180414" "20180415")
 cycs=("00" "06" "12" "18")
 obstypes=("insitu" "sss" "adt" "icec" "sst")
 
-# Convert cdl files into nc for all cycles and obstypes
+# Copy marine obs (in situ BUFR and satellite retrieved netCDF files) to DMPDIR
 for PDY in "${PDYs[@]}"; do
     PDYdir="gdas.${PDY}"
     for cyc in "${cycs[@]}"; do
         for obstype in "${obstypes[@]}"; do
             if [ $obstype != "insitu" ]; then
                 fullsubdir="$PDYdir/$cyc/ocean/$obstype"
-            else
-                fullsubdir="$PDYdir/$cyc/atmos"
-            fi
-            mkdir -p "$fullsubdir"
+                mkdir -p "$fullsubdir"
 
-            indir="${testdatadir}/${fullsubdir}"
-            for file in "$indir"/*.cdl; do
-                if [ -f "$file" ]; then
-                    filename=$(basename -- "$file")
-                    filename_noext="${filename%.cdl}"
-                    ncgen -o "$fullsubdir/${filename_noext}.nc" "$file"
-                fi
-            done
+                # Convert cdl files into nc for all cycles and obstypes
+                indir="${testdatadir}/${fullsubdir}"
+                for file in "$indir"/*.cdl; do
+                    if [ -f "$file" ]; then
+                        filename=$(basename -- "$file")
+                        filename_noext="${filename%.cdl}"
+                        ncgen -o "$fullsubdir/${filename_noext}.nc" "$file"
+                    fi
+                done
+            else
+                # Copy subsampled monthly in situ BUFR
+                # Example filename: tesac.201804.dcom_subsampled
+                # TODO: SP to replace these with daily and monthly subsampled with proper dates 
+                fullsubdir="$PDYdir/$cyc/atmos"
+                mkdir -p "$fullsubdir"
+                for file in "$testdatadir_bufr/*.${PDY:0:6}.dcom_subsampled"; do
+                    cp -p $file $fullsubdir
+                done
+            fi
         done
     done
 done
-#echo "Copy subsampled BUFR into atmos directory..."
-#for f in ${project_source_dir}/build/gdas/test/testdata/*_subsampled; do
-#    cp -p $f atmos/
-#done
