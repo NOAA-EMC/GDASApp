257cef4b175c7492e6a9676f093a0cd340e90d3b
AndrewEichmann-NOAA <andrew.eichmann@noaa.gov>
2024-02-15 16:48:48 +0000

    initial OO-correctish stubs

---
+++ scripts/exgdas_global_marine_analysis_ecen.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# exgdas_global_marine_analysis_ecen.py
+# This script creates an MarineRecenter class
+# and runs the initialize, run, and finalize  methods
+# which currently are stubs
+import os
+
+from wxflow import Logger, cast_strdict_as_dtypedict
+from soca.marine_recenter import MarineRecenter
+
+# Initialize root logger
+logger = Logger(level='DEBUG', colored_log=True)
+
+
+if __name__ == '__main__':
+
+    # Take configuration from environment and cast it as python dictionary
+    config = cast_strdict_as_dtypedict(os.environ)
+
+    # Instantiate the aerosol analysis task
+    MarineRecen = MarineRecenter(config)
+    MarineRecen.initialize()
+    MarineRecen.execute()
+    MarineRecen.finalize()
---
+++ ush/soca/marine_recenter.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import os
+from logging import getLogger
+from typing import Dict, List, Any
+from wxflow import logit
+
+logger = getLogger(__name__.split('.')[-1])
+
+class MarineRecenter():
+
+  def __init__(self, config):
+    logger.info("init")
+
+  @logit(logger)
+  def initialize(self):
+    logger.info("initialize")
+
+  @logit(logger)
+  def execute(self):
+    logger.info("execute")
+
+  @logit(logger)
+  def finalize(self):
+    logger.info("finalize")
+
+
+
