6e80e580f904dc9de502f4644392d06d2fb47f30
Cory Martin <cory.r.martin@noaa.gov>
2024-02-16 14:16:43 -0500

    Account for ones that already were double curly but no spaces

--- ush/modify_yaml_syntax.py
+++ ush/modify_yaml_syntax.py
@@ -11,6 +11,16 @@ def get_word_in_dollar_parentheses(text):
     return matches
 
 
+def get_word_inside_curly_braces(text):
+    pattern = r'{{(.*?)}}'
+    matches = re.findall(pattern, text)
+    return matches
+
+
+def has_blank_spaces(text):
+    return ' ' in text
+
+
 with open(fname, 'r') as file:
     filedata = file.readlines()
 # do stuff
@@ -18,10 +28,15 @@ newdata = []
 for line in filedata:
     matches = get_word_in_dollar_parentheses(line)
     if len(matches) > 0:
-        print(line)
         for match in matches:
+            print(match)
             line = line.replace("$(%s)" % match, "{{ %s }}" % match)
-        print(line)
+    matches = get_word_inside_curly_braces(line)
+    if len(matches) > 0:
+        for match in matches:
+            if not has_blank_spaces(match):
+                print(match)
+                line = line.replace("{{%s}}" % match, "{{ %s }}" % match)
     newdata.append(line)
 
 # write out file
