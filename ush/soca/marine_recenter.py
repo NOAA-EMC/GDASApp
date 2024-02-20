3ca7f0beb5cde24214e432cd2eb778923dbc7182
AndrewEichmann-NOAA <andrew.eichmann@noaa.gov>
2024-02-20 16:51:25 +0000

    add some documentation

--- ush/soca/marine_recenter.py
+++ ush/soca/marine_recenter.py
@@ -2,45 +2,91 @@
 
 import os
 from logging import getLogger
-from typing import Dict, List, Any
-from wxflow import AttrDict, FileHandler, logit, Task
+from wxflow import Dict, logit, Task, YAMLFile
+import ufsda
 
 logger = getLogger(__name__.split('.')[-1])
 
 
 class MarineRecenter(Task):
+    """
+    Class for global ocean analysis recentering task
+    """
+
 
     @logit(logger, name="MarineRecenter")
-    def __init__(self, config):
+    def __init__(self, config: Dict) -> None:
+    """Constructor for ocean recentering task
+    Parameters:
+    ------------
+    config: Dict
+        configuration, namely evironment variables
+    Returns:
+    --------
+    None 
+    """
+
         logger.info("init")
         super().__init__(config)
 
-        # Create a local dictionary that is repeatedly used across this class
-        local_dict = AttrDict(
-            {
-                "diags": os.path.join(self.runtime_config['DATA'], 'diags'),            # output dir for soca DA obs space
-                "obs_in": os.path.join(self.runtime_config['DATA'], 'obs') ,            # input      "           "
-                "bkg_dir": os.path.join(self.runtime_config['DATA'], 'bkg'),            # ice and ocean backgrounds
-                "anl_out": os.path.join(self.runtime_config['DATA'], 'Data'),           # output dir for soca DA
-                "static_ens": os.path.join(self.runtime_config['DATA'], 'static_ens')  # clim. ens.
-            }
-        )
+        # Variables of convenience
+	gdas_home = os.path.join(config['HOMEgfs'], 'sorc', 'gdas.cd')
+        half_assim_freq = timedelta(hours=int(config['assim_freq'])/2)
+        window_middle = datetime.strptime(config['PDY')+config['cyc'], '%Y%m%d%H')
+        window_begin = datetime.strptime(config['PDY')+config['cyc'], '%Y%m%d%H') - half_assim_freq
+        window_begin_iso = window_begin.strftime('%Y-%m-%dT%H:%M:%SZ')
+        window_middle_iso = window_middle.strftime('%Y-%m-%dT%H:%M:%SZ')
+        fcst_begin = datetime.strptime(config['PDY')+config['cyc'), '%Y%m%d%H')
 
-        # task_config is everything that this task should need
-        self.task_config = AttrDict(**self.config, **self.runtime_config, **local_dict)
+        self.window_config = {'window_begin': f"{window_begin.strftime('%Y-%m-%dT%H:%M:%SZ')}",
+             'ATM_WINDOW_BEGIN': window_begin_iso,
+             'ATM_WINDOW_MIDDLE': window_middle_iso,
+             'ATM_WINDOW_LENGTH': f"PT{config['assim_freq')}H"}
 
-        #_soca_ensb_yaml_temp 
-        print(self.task_config) 
+        _stage_cfg = YAMLFile(path=os.path.join(gdas_home, 'parm', 'templates', 'stage.yaml'))
+        _stage_cfg = Template.substitute_structure(_stage_cfg, TemplateConstants.DOUBLE_CURLY_BRACES, self.window_config.get)
+        _stage_cfg = Template.substitute_structure(_stage_cfg, TemplateConstants.DOLLAR_PARENTHESES, self.window_config.get)
+        self.stage_cfg = _stage_cfg
+
 
     @logit(logger)
     def initialize(self):
+    """Method initialize for ocean recentering task
+    Parameters:
+    ------------
+    None 
+    Returns:
+    --------
+    None 
+    """
+
         logger.info("initialize")
+
+        ufsda.stage.soca_fix(self.stage_cfg)
 
 
     @logit(logger)
     def run(self):
+    """Method run for ocean recentering task
+    Parameters:
+    ------------
+    None 
+    Returns:
+    --------
+    None 
+    """
+
         logger.info("run")
 
     @logit(logger)
     def finalize(self):
+    """Method finalize for ocean recentering task
+    Parameters:
+    ------------
+    None 
+    Returns:
+    --------
+    None 
+    """
+
         logger.info("finalize")
