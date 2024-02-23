2f2dd21030e5bd52172c6401ba407cc2ba03ec79
AndrewEichmann-NOAA <andrew.eichmann@noaa.gov>
2024-02-23 17:32:20 +0000

    points for style

--- scripts/exgdas_global_marine_analysis_prep.py
+++ scripts/exgdas_global_marine_analysis_prep.py
@@ -259,10 +259,10 @@ config.save(diffu_vt_yaml)
 logger.info(f"---------------- generate var.yaml")
 var_yaml = os.path.join(anl_dir, 'var.yaml')
 var_yaml_template = os.path.join(gdas_home,
-                                 'parm',
-                                 'soca',
-                                 'variational',
-                                 '3dvarfgat.yaml')
+                                  'parm',
+                                  'soca',
+                                  'variational',
+                                  '3dvarfgat.yaml')
 bkg_utils.gen_bkg_list(bkg_path=os.getenv('COM_OCEAN_HISTORY_PREV'),
              out_path=bkg_dir,
              window_begin=window_begin,
