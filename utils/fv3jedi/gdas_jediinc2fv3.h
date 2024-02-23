e0556f540a449668f9c54be68a0aaace7ab552f9
DavidNew-NOAA <david.new@noaa.gov>
2024-02-23 20:34:32 +0000

    Bug fixes

--- utils/fv3jedi/gdas_jediinc2fv3.h
+++ utils/fv3jedi/gdas_jediinc2fv3.h
@@ -27,25 +27,33 @@ namespace gdasapp {
     static const std::string classname() {return "gdasapp::jediinc2fv3";}
 
     int execute(const eckit::Configuration & fullConfig, bool validate) const {
-      // Setup geometry
-      const eckit::LocalConfiguration geomConfig(fullConfig, "geometry");
-      const fv3jedi::Geometry geom(geomConfig, this->getComm());
-      oops::Log::info() << "Geometry: " << std::endl << geom << std::endl;
+      // Setup variable change
+      const eckit::LocalConfiguration varChangeConfig(fullConfig, "variable change");
+      oops::Variables varin(varChangeConfig, "input variables");
+      oops::Variables varout(varChangeConfig, "output variables");
 
-      // Setup background state
-      const eckit::LocalConfiguration stateConfig(fullConfig, "state");
-      fv3jedi::State xxBkg(geom, stateConfig);
-      oops::Log::info() << "background: " << std::endl << xxBkg << std::endl;
+      // Setup background
+      const eckit::LocalConfiguration bkgConfig(fullConfig, "background");
+      const eckit::LocalConfiguration stateGeomConfig(bkgConfig, "geometry");
+      const eckit::LocalConfiguration stateInputConfig(bkgConfig, "input");
+      const fv3jedi::Geometry stateGeom(stateGeomConfig, this->getComm());
+      fv3jedi::State xxBkg(stateGeom, stateInputConfig);
+      oops::Log::info() << "Background Geometry: " << std::endl << stateGeom << std::endl;
+      oops::Log::info() << "Background State: " << std::endl << xxBkg << std::endl;
 
       // Setup increment
-      oops::Variables incrConfig(fullConfig, "increment");
-      const fv3jedi::Increment dx(geom, incrConfig, xxBkg.validTime());
+      const eckit::LocalConfiguration incrConfig(fullConfig, "increment");
+      const eckit::LocalConfiguration incrGeomConfig(incrConfig, "geometry");
+      const eckit::LocalConfiguration incrInputConfig(incrConfig, "input");
+      const fv3jedi::Geometry incrGeom(incrGeomConfig, this->getComm());
+      fv3jedi::Increment dx(incrGeom, varin, xxBkg.validTime());
+      dx.read(incrInputConfig);
+      oops::Log::info() << "Increment Geometry: " << std::endl << incrGeom << std::endl;
       oops::Log::info() << "Increment: " << std::endl << dx << std::endl;
 
-      // Setup variables change
+      //
       std::unique_ptr<fv3jedi::VariableChange> vc;
-      const eckit::LocalConfiguration varChangeConfig(fullConfig, "variable change");
-      oops::Variables varout(varChangeConfig, "output variables");
+      vc.reset(new fv3jedi::VariableChange(varChangeConfig, stateGeom));
 
       // Setup output config
       const eckit::LocalConfiguration outputConfig(fullConfig, "output");
@@ -53,7 +61,7 @@ namespace gdasapp {
       // ----------------------------------------------------------------------------
 
       // Add increment to background to get analysis
-      fv3jedi::State xxAnl(geom, xxBkg);
+      fv3jedi::State xxAnl(stateGeom, xxBkg);
       xxAnl += dx;
 
       // Perform variables change on background and analysis
@@ -61,7 +69,7 @@ namespace gdasapp {
       vc->changeVar(xxAnl, varout);
 
       // Get final FV3 increment
-      fv3jedi::Increment dxFV3(geom, incrConfig, xxBkg.validTime());
+      fv3jedi::Increment dxFV3(incrGeom, varout, xxBkg.validTime());
       dxFV3.diff(xxAnl, xxBkg);
       oops::Log::info() << "FV3 increment: " << std::endl << dx << std::endl;
 
