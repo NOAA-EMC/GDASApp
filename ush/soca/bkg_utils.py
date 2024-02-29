807cd2ccc19aab746ccd40df2f5d2c9ab0c6ae86
AndrewEichmann-NOAA <andrew.eichmann@noaa.gov>
2024-02-29 19:38:32 +0000

    style

--- ush/soca/bkg_utils.py
+++ ush/soca/bkg_utils.py
@@ -168,7 +168,7 @@ def rename_bkg(bkg_dir, anl_dir, RUN):
     mom_ic_src = glob.glob(os.path.join(bkg_dir, f'{RUN}.ocean.*.inst.f003.nc'))[0]
     mom_ic_dst = os.path.join(anl_dir, 'INPUT', 'MOM.res.nc')
     ics_list.append([mom_ic_src, mom_ic_dst])
-   
+
     # seaice IC's
     cice_ic_src = glob.glob(os.path.join(bkg_dir, f'{RUN}.agg_ice.*.inst.f003.nc'))[0]
     cice_ic_dst = os.path.join(anl_dir, 'INPUT', 'cice.res.nc')
