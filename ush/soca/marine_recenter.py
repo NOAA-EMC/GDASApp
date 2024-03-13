7b53b2c539a93e4e85b012c6e0d4ae4f8e41440b
AndrewEichmann-NOAA <andrew.eichmann@noaa.gov>
2024-03-13 13:57:04 +0000

    correct path parens

--- ush/soca/marine_recenter.py
+++ ush/soca/marine_recenter.py
@@ -234,7 +234,7 @@ class MarineRecenter(Task):
                                    f'{cyc}',
                                    f'mem{str(mem).zfill(3)}',
                                    'analysis',
-                                   'ocean',
+                                   'ocean')
             mem_dir_real = os.path.realpath(mem_dir)
             mem_dir_list.append(mem_dir_real)
 
