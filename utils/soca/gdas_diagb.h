d8007eeab14eb4d62f8b2a423ac83e79e7a76dc9
Guillaume Vernieres <guillaume.vernieres@gmail.com>
2024-03-20 14:26:35 +0000

    more options

--- utils/soca/gdas_diagb.h
+++ utils/soca/gdas_diagb.h
@@ -25,6 +25,7 @@
 #include "oops/util/DateTime.h"
 #include "oops/util/Duration.h"
 #include "oops/util/FieldSetHelpers.h"
+#include "oops/util/FieldSetOperations.h"
 #include "oops/util/Logger.h"
 
 #include "soca/Geometry/Geometry.h"
@@ -49,19 +50,18 @@ namespace gdasapp {
 
       oops::Log::info() << "====================== date" << std::endl;
       /// Get the date
+      // -------------
       std::string strdt;
       fullConfig.get("date", strdt);
       util::DateTime cycleDate = util::DateTime(strdt);
 
       /// Get the list of variables
+      // --------------------------
       oops::Log::info() << "====================== variables" << std::endl;
       oops::Variables socaVars(fullConfig, "variables.name");
 
-      /// Read rescaling factor
-      double rescale;
-      fullConfig.get("rescale", rescale);
-
       /// Read the background
+      // --------------------
       oops::Log::info() << "====================== read bkg" << std::endl;
       soca::State xb(geom, socaVars, cycleDate);
       const eckit::LocalConfiguration bkgConfig(fullConfig, "background");
@@ -70,11 +70,12 @@ namespace gdasapp {
       xb.toFieldSet(xbFs);
 
       /// Create the mesh connectivity (Copy/paste of Francois's stuff)
+      // --------------------------------------------------------------
       // Build edges, then connections between nodes and edges
       int nbHalos(2);
       fullConfig.get("number of halo points", nbHalos);
       int nbNeighbors(4);
-      fullConfig.get("number of halo points", nbHalos);
+      fullConfig.get("number of neighbors", nbNeighbors);
       atlas::functionspace::NodeColumns nodeColumns = geom.functionSpace();
       atlas::Mesh mesh = nodeColumns.mesh();
       atlas::mesh::actions::build_edges(mesh);
@@ -108,6 +109,8 @@ namespace gdasapp {
       };
 
       /// Compute local std. dev. as a proxy of the bkg error
+      // ----------------------------------------------------
+      // Identify halo nodes
       const auto ghostView =
         atlas::array::make_view<int, 1>(geom.functionSpace().ghost());
 
@@ -117,9 +120,17 @@ namespace gdasapp {
       atlas::FieldSet bkgErrFs;
       bkgErr.toFieldSet(bkgErrFs);
 
-      // Loop through variables
+      // Get the std dev to add to sst
+      double sstBkgErrMin(0.0);
+      fullConfig.get("min sst", sstBkgErrMin);
+
+      // Get the max std dev for ssh
+      double sshMax(0.0);
+      fullConfig.get("max ssh", sshMax);
+
       auto h = atlas::array::make_view<double, 2>(xbFs["hocn"]);
 
+      // Loop through variables
       for (auto & var : socaVars.variables()) {
         // Skip the layer thickness variable
         if (var == "hocn") {
@@ -129,113 +140,167 @@ namespace gdasapp {
         auto bkg = atlas::array::make_view<double, 2>(xbFs[var]);
         auto stdDevBkg = atlas::array::make_view<double, 2>(bkgErrFs[var]);
 
-        int nbz = 2;  // Number of closest point in the vertical, above and below
-        int nzMld = 10;  // Vertical index for the mixed layer depth
-        nbz = std::min(nbz, xbFs[var].shape(1) - 1);
-        for (atlas::idx_t level = 0; level < xbFs[var].shape(1) - nbz; ++level) {
-          oops::Log::info() << "                       level: " << level << std::endl;
-          for (atlas::idx_t jnode = 0; jnode < xbFs[var].shape(0); ++jnode) {
-            // Early exit if thickness is 0 or on a ghost cell
-            if (ghostView(jnode) > 0 || abs(h(jnode, 0)) <= 0.1) {
-              continue;
-            }
+        // Loop through nodes
+        for (atlas::idx_t jnode = 0; jnode < xbFs[var].shape(0); ++jnode) {
+          // Early exit if thickness is 0 or on a ghost cell
+          if (ghostView(jnode) > 0 || abs(h(jnode, 0)) <= 0.1) {
+            continue;
+          }
 
-            // Ocean or ice node, do something
+          // get indices of neighbor cells
+          auto neighbors = get_neighbors_of_node(jnode);
+          int nbh = neighbors.size();
+
+          // 2D case
+          if (xbFs[var].shape(1) == 1) {
             std::vector<double> local;
-            auto neighbors = get_neighbors_of_node(jnode);
-            int nbh = neighbors.size();
             for (int nn = 0; nn < neighbors.size(); ++nn) {
-              int nbNode = neighbors[nn];
-              int levelMin = level - nbz;
-              int levelMax = level + nbz;
-              if (level < nzMld) {
-                int levelMin = 0;
-                int levelMax = nzMld;
+              if ( abs(h(neighbors[nn], 0)) <= 0.1 ) {
+                continue;
               }
-              for (int ll = levelMin; ll <= levelMax; ++ll) {
-                if ( abs(h(nbNode, ll)) <= 0.1 ) {
-                  continue;
-                }
-                local.push_back(bkg(nbNode, ll));
-              }
+              local.push_back(bkg(neighbors[nn], 0));
             }
-
             //Set the minimum number of points
-            int minn = 12;  /// probably should be passed through the config
-            if (xbFs[var].shape(1) == 1) {
-              minn = 4;
-            }
+            int minn = 4;  /// probably should be passed through the config
             if (local.size() >= minn) {
               // Mean
               double mean = std::accumulate(local.begin(), local.end(), 0.0) / local.size();
-
               // Standard deviation
               double stdDev(0.0);
               for (int nn = 0; nn < nbh; ++nn) {
                 stdDev += std::pow(local[nn] - mean, 2.0);
               }
-              if (stdDev > 0.0 || local.size() != 0) {
-                stdDevBkg(jnode, level)  = rescale * std::sqrt(stdDev / (local.size() - 1));
-              }
 
-              // Extrapolate upper levels
-              double meanMld = std::accumulate(local.begin(), local.begin() + nzMld, 0.0) / nzMld;
-              for (int ll = 0; ll < nzMld; ++ll) {
-                stdDevBkg(jnode, ll) = stdDevBkg(jnode, nbz);
-                //stdDevBkg(jnode, ll) = meanMld;
+              if (stdDev > 0.0 || local.size() > 2) {
+                stdDevBkg(jnode, 0)  = std::sqrt(stdDev / (local.size() - 1));
+              }
+              // Filter out ssh std. dev.
+              if (var == "ssh") {
+                stdDevBkg(jnode, 0) = std::min(stdDevBkg(jnode, 0), sshMax);
               }
             }
-          }
-        }
-      }
+            continue;  // early exit, no need to loop through levels
+          } else {
+            // 3D case
+            int nbz = 1;  // Number of closest point in the vertical, above and below
+            int nzMld = 10;  // Vertical index for the mixed layer depth, hard-wired to the MOM6 UFS config
+            for (atlas::idx_t level = 0; level < xbFs[var].shape(1) - nbz; ++level) {
+              std::vector<double> local;
+              for (int nn = 0; nn < neighbors.size(); ++nn) {
+                int levelMin = std::max(0, level - nbz);
+                int levelMax = level + nbz;
+                if (level < nzMld) {
+                  // If in the MLD, compute the std. dev. throughout the MLD water column
+                  levelMin = 0;
+                  levelMax = nzMld;
+                }
+                for (int ll = levelMin; ll <= levelMax; ++ll) {
+                  if ( abs(h(neighbors[nn], ll)) <= 0.1 ) {
+                    continue;
+                  }
+                  local.push_back(bkg(neighbors[nn], ll));
+                }
+              }
+              //Set the minimum number of points
+              int minn = 12;  /// probably should be passed through the config
+              if (local.size() >= minn) {
+                // Mean
+                double mean = std::accumulate(local.begin(), local.end(), 0.0) / local.size();
+
+                // Standard deviation
+                double stdDev(0.0);
+                for (int nn = 0; nn < nbh; ++nn) {
+                  stdDev += std::pow(local[nn] - mean, 2.0);
+                }
+                // Setup the additive variance (only used ofr sst)
+                double additiveStdDev(0.0);
+                if (var == "tocn" & level == 0) {
+                    additiveStdDev = sstBkgErrMin;
+                  }
+                if (stdDev > 0.0 || local.size() > 2) {
+                  stdDevBkg(jnode, level)  = std::sqrt(stdDev / (local.size() - 1)) + additiveStdDev;
+                }
+              }
+            }  // end level
+          }  // end 3D case
+        }  // end jnode
+      }  // end var
 
       // TODO(G): Assume that the steric balance explains 97% of ssh ... or do it properly ... maybe
 
+
       /// Smooth the fields
-      int niter(0);
-      fullConfig.get("smoothing iterations", niter);
-      for (auto & var : socaVars.variables()) {
-        // Skip the layer thickness variable
-        if (var == "hocn") {
-          continue;
-        }
+      // ------------------
+      if (fullConfig.has("simple smoothing")) {
+        int niter(0);
+        fullConfig.get("simple smoothing.horizontal iterations", niter);
+        for (auto & var : socaVars.variables()) {
+          // Skip the layer thickness variable
+          if (var == "hocn") {
+            continue;
+          }
 
-        for (int iter = 0; iter < niter; ++iter) {
+          // Horizontal averaging
+          for (int iter = 0; iter < niter; ++iter) {
 
-          // Update the halo points
-          nodeColumns.haloExchange(bkgErrFs[var]);
-          auto stdDevBkg = atlas::array::make_view<double, 2>(bkgErrFs[var]);
+            // Update the halo points
+            nodeColumns.haloExchange(bkgErrFs[var]);
+            auto stdDevBkg = atlas::array::make_view<double, 2>(bkgErrFs[var]);
 
-          // Loops through nodes and levels
-          for (atlas::idx_t level = 0; level < xbFs[var].shape(1); ++level) {
-            for (atlas::idx_t jnode = 0; jnode < xbFs["tocn"].shape(0); ++jnode) {
-              // Early exit if thickness is 0 or on a ghost cell
-              if (abs(h(jnode, 0)) <= 0.1) {
-                continue;
-              }
-
-              // Ocean or ice node, do something
-              std::vector<double> local;
-              auto neighbors = get_neighbors_of_node(jnode);
-              int nbh = neighbors.size();
-              for (int nn = 0; nn < neighbors.size(); ++nn) {
-                int nbNode = neighbors[nn];
-                if ( abs(h(nbNode, level)) <= 0.1 ) {
+            // Loops through nodes and levels
+            for (atlas::idx_t level = 0; level < xbFs[var].shape(1); ++level) {
+              for (atlas::idx_t jnode = 0; jnode < xbFs["tocn"].shape(0); ++jnode) {
+                // Early exit if thickness is 0 or on a ghost cell
+                if (abs(h(jnode, level)) <= 0.1) {
                   continue;
                 }
-                local.push_back(stdDevBkg(nbNode, level));
-              }
 
-              if (local.size() > 2) {
-                stdDevBkg(jnode, level) = std::accumulate(local.begin(), local.end(), 0.0) / local.size();
+                // Ocean or ice node, do something
+                std::vector<double> local;
+                auto neighbors = get_neighbors_of_node(jnode);
+                int nbh = neighbors.size();
+                for (int nn = 0; nn < neighbors.size(); ++nn) {
+                  int nbNode = neighbors[nn];
+                  if ( abs(h(nbNode, level)) <= 0.1 ) {
+                    continue;
+                  }
+                  local.push_back(stdDevBkg(nbNode, level));
+                }
+
+                if (local.size() > 2) {
+                  stdDevBkg(jnode, level) = std::accumulate(local.begin(), local.end(), 0.0) / local.size();
+                }
+              }
+            }
+          }
+
+          // Vertical averaging
+          if (xbFs[var].shape(1) == 1) {
+            oops::Log::info() << "skipping vertical smoothing for " << var << std::endl;
+          } else {
+            int niterVert(0);
+            fullConfig.get("simple smoothing.vertical iterations", niterVert);
+
+            auto stdDevBkg = atlas::array::make_view<double, 2>(bkgErrFs[var]);
+            auto tmpArray(stdDevBkg);
+            for (int iter = 0; iter < niterVert; ++iter) {
+              for (atlas::idx_t jnode = 0; jnode < xbFs["tocn"].shape(0); ++jnode) {
+                for (atlas::idx_t level = 1; level < xbFs[var].shape(1)-1; ++level) {
+                  stdDevBkg(jnode, level) = ( tmpArray(jnode, level-1) +
+                                              tmpArray(jnode, level) +
+                                              tmpArray(jnode, level+1) ) / 3.0;
+                }
+                stdDevBkg(jnode, 0) = stdDevBkg(jnode, 1);
               }
             }
           }
         }
       }
 
-      /// Smooth the background error
-      // That doesn't seem to work, the output is in [0, ~1000]
+      /// Use explicit diffusion to smooth the background error
+      // ------------------------------------------------------
+      // TODO: Use this once Travis adds the option to skip the normalization.
+      //       The output is currently in [0, ~1000]
       // Initialize the diffusion central block
       if (fullConfig.has("diffusion")) {
         const eckit::LocalConfiguration diffusionConfig(fullConfig, "diffusion");
@@ -252,6 +317,11 @@ namespace gdasapp {
         diffuse.multiply(dx);
         bkgErrFs = dx.fieldSet();
       }
+
+      // Rescale
+      double rescale;
+      fullConfig.get("rescale", rescale);
+      util::multiplyFieldSet(bkgErrFs, rescale);
 
       // We want to write with soca, not atlas: Syncronize with soca Increment
       bkgErr.fromFieldSet(bkgErrFs);
