5f15a89af046a34e7707c68a6fc9aa6601aae335
Katherine Lukens <Katherine.Lukens@noaa.gov>
2024-03-25 21:01:17 +0000

    Added code to replace bkg SST with bkg skin T

--- ush/soca/bkg_utils.py
+++ ush/soca/bkg_utils.py
@@ -11,6 +11,7 @@ import shutil
 from wxflow import (Logger, FileHandler)
 import xarray as xr
 import yaml
+from scipy.interpolate import griddata
 
 logger = Logger()
 
@@ -89,6 +90,93 @@ def test_hist_date(histfile, ref_date):
     logger.info(f"*** history file date: {hist_date} expected date: {ref_date}")
     assert hist_date == ref_date, 'Inconsistent bkg date'
 
+#-------------------------------------------------
+# Added by K. Lukens
+
+def sst2skint(bkg_path, bkg_file, gcyc, fcst_hr):
+    """
+    Replace SST background (top layer of ocean model) with skin T background (from atmosphere)
+    """
+
+    # Get paths to background files
+    #tpath_bkg_ocn = str(os.getenv('COM_OCEAN_HISTORY_PREV'))
+    path_bkg_ocn  = str(bkg_path)+'/' #tpath_bkg_ocn+'/'
+    path_bkg_atm = path_bkg_ocn.replace('ocean', 'atmos')
+    #os.system('mkdir '+str(path_bkg_atm))
+
+    #tmpatmpath = '/scratch1/NCEPDEV/da/Katherine.Lukens/NSST/tools/sst2skinT/data/model_data/atmos/history/gdas.t'+str(gcyc)+'z.sfcf'+str(fcst_hr)+'.nc'
+    tmpatmpath = '/scratch1/NCEPDEV/da/Katherine.Lukens/NSST/tools/sst2skinT/data/model_data/atmos/history/gdas.t12z.sfcf'+str(fcst_hr)+'.nc'
+    os.system('cp '+str(tmpatmpath)+' '+str(path_bkg_atm))
+
+    # Get ocean model grid
+#    grid_fname = os.path.join('/scratch2/NCEPDEV/ocean/Guillaume.Vernieres/runs/low-res/soca_gridspec.nc')
+    grid_fname = os.path.join('/scratch1/NCEPDEV/da/Katherine.Lukens/NSST/tools/sst2skinT/data/model_data/MOM.res.nc')
+#    grid_fname = os.path.join('../../build/gdas/test/soca/gw/testrun/testjjobs/RUNDIRS/gdas_test/gdasocnanal_12/INPUT/MOM.res.nc')
+    ds = xr.open_dataset(grid_fname)
+#    ocnlat = np.squeeze(ds['lat'][:])
+#    ocnlon = np.squeeze(ds['lon'][:])
+    ocnlat = np.squeeze(ds['geolat'][:])
+    ocnlon = np.squeeze(ds['geolon'][:])
+    ds.close()
+    	# ocean grid size
+    shapelat = np.shape(ocnlat)
+    nlat = shapelat[0]
+    nlon = shapelat[1]
+
+    # Get skin T background
+    fskin = path_bkg_atm+'gdas.t12z.sfcf'+str(fcst_hr)+'.nc'
+    #fskin = os.path.join(path_bkg_atm, f'gdas.t12z.sfcf'+str(fcst_hr)+'.nc')
+    print("fskin = "+str(fskin))
+    #ds     = xr.open_dataset(fskin)
+    #skinT  = np.squeeze(ds['tmpsfc'][0,:,:].values) - 273.15
+    #atmlat = np.squeeze(ds['grid_yt'].values)
+    #atmlon = np.squeeze(ds['grid_xt'].values)
+    ds     = Dataset(fskin)
+    skinT  = np.array(ds.variables['tmpsfc'][0,:,:] - 273.15)
+    atmlat = np.array(ds.variables['grid_yt'])
+    atmlon = np.array(ds.variables['grid_xt'])
+    #atmlon = ds.variables['grid_xt']
+    ds.close()
+    print("type atmlat = "+str(type(atmlat)))
+    print("type atmlon = "+str(type(atmlon)))
+    print("shape atmlat = "+str(np.shape(atmlat)))
+    print("shape atmlon = "+str(np.shape(atmlon)))
+
+    # Regrid skin T to ocean model grid
+    ocnlon_max = np.max(ocnlon)
+        # get atmos gaussian grid and rotate to match ocean grid
+    #atmlon[atmlon >= ocnlon_max] = atmlon[atmlon >= ocnlon_max] - 360
+    #atmlon = np.where(atmlon >= ocnlon_max, atmlon - 360, atmlon)
+    atmlon = [atmlon[i]-360.0 if atmlon[i]>=ocnlon_max else atmlon[i] for i in range(np.size(atmlon))]
+
+    atmlongrid, atmlatgrid = np.meshgrid(atmlon, atmlat)
+        # regrid atmos variable to ocean grid
+    skinT_regrid = griddata((atmlongrid.reshape(-1), atmlatgrid.reshape(-1)), np.squeeze(skinT.reshape(-1)), (ocnlon, ocnlat), method='nearest')
+
+    # Replace SST with skin T
+    # ... Add new T (with skin T) back into existing ocean background files (netcdf)
+        # open ocean background file
+    nc = Dataset(path_bkg_ocn+bkg_file, mode='a', format='NetCDF4 Classic')
+        # mask land values in skinT_regrid
+    sst = np.asarray(nc.variables['Temp'][0,0,:,:])
+    for j in range(nlat-1):
+      for k in range(nlon-1):
+        #if sst[j,k]<0:
+        if sst[j,k]<-999 or sst[j,k]>999:
+          tmiss = sst[j,k]
+          break
+    print("sst2skint: land missing value = "+str(tmiss))
+    skinT_regridmask = np.where(sst==tmiss, tmiss, skinT_regrid)
+    print("sst2skint: shape regrid, regridmask = "+str(np.shape(skinT_regrid))+" "+str(np.shape(skinT_regridmask)))
+    print("sst2skint: min, max regrid = "+str(np.min(skinT_regrid))+" "+str(np.max(skinT_regrid)))
+    print("sst2skint: min, max regridmask = "+str(np.min(skinT_regridmask))+" "+str(np.max(skinT_regridmask)))
+        # replace SST with skinT
+    nc['Temp'][0,0,:,:] = skinT_regridmask[:,:]
+    newncT = np.asarray(nc.variables['Temp'])
+
+    nc.close()
+
+#-------------------------------------------------
 
 def gen_bkg_list(bkg_path, out_path, window_begin=' ', yaml_name='bkg.yaml', ice_rst=False):
     """
@@ -108,6 +196,22 @@ def gen_bkg_list(bkg_path, out_path, window_begin=' ', yaml_name='bkg.yaml', ice
     files = []
     for fcst_hr in fcst_hrs:
         files.append(os.path.join(bkg_path, f'{RUN}.ocean.t'+gcyc+'z.inst.f'+str(fcst_hr).zfill(3)+'.nc'))
+
+	#------------------------------------------------------
+	# K. Lukens:
+ 	# Add SST-to-skinT function call here
+		# make a copy of the background file
+        #os.system('cp '+str(bkg_path)+'{RUN}.ocean.t'+gcyc+'z.inst.f'+str(fcst_hr).zfill(3)+'.nc {RUN}.ocean.t'+gcyc+'z.inst.f'+str(fcst_hr).zfill(3)+'_CP.nc')
+
+                # create /atmos/history/ directory
+        path_bkg_ocn = str(bkg_path)+'/' #tpath_bkg_ocn+'/'
+        path_bkg_atm = path_bkg_ocn.replace('ocean', 'atmos')
+        os.system('mkdir -p '+str(path_bkg_atm))
+
+                # replace SST background with skin T background in the existing netcdf file
+        bkg_file = str(RUN)+'.ocean.t'+str(gcyc)+'z.inst.f'+str(fcst_hr).zfill(3)+'.nc'
+        sst2skint(bkg_path, bkg_file, gcyc, str(fcst_hr).zfill(3))
+	#------------------------------------------------------
 
     # Identify the ocean background that will be used for the  vertical coordinate remapping
     ocn_filename_ic = os.path.splitext(os.path.basename(files[0]))[0]+'.nc'
@@ -172,4 +276,22 @@ def stage_ic(bkg_dir, anl_dir, RUN, gcyc):
     cice_ic_src = os.path.join(bkg_dir, f'{RUN}.agg_ice.t{gcyc}z.inst.f003.nc')
     cice_ic_dst = os.path.join(anl_dir, 'INPUT', 'cice.res.nc')
     ics_list.append([cice_ic_src, cice_ic_dst])
+
+    #------------------------------------------------------
+    # K. Lukens:
+    # Add SST-to-skinT function call here
+            # make a copy of the background file
+    #mom_ic_src_cp = os.path.join(bkg_dir, f'{RUN}.ocean.t{gcyc}z.inst.f003_CP.nc')
+    #os.system('cp '+str(mom_ic_src)+' '+str(mom_ic_src_cp))
+
+            # create /atmos/history/ directory
+    #path_bkg_ocn = str(bkg_dir)+'/' #tpath_bkg_ocn+'/'
+    #path_bkg_atm = path_bkg_ocn.replace('ocean', 'atmos')
+    #os.system('mkdir -p '+str(path_bkg_atm))
+
+            # replace SST background with skin T background in the existing netcdf file
+    bkg_file = f'{RUN}.ocean.t{gcyc}z.inst.f003.nc'
+    sst2skint(bkg_dir, bkg_file, gcyc, '003')
+    #------------------------------------------------------
+
     FileHandler({'copy': ics_list}).sync()
