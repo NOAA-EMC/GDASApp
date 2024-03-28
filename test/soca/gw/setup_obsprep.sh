33b4cb99817c15b29433dc8a44497268ce179af4
Shastri Paturi <shastri.paturi@noaa.gov>
2024-03-28 12:03:06 -0400

    Add BUFR2IODA python API converter to prepoceanobs task (#914)
    This PR adds BUFR2IODA python API converter to prepoceanobs task and
    does the following:
    1. Subsampled monthly bathy, tesac and trkob in situ BUFR files are
    tested.
    2. The subsampled files have been renamed (from
    <obtype>.201804.dcom_subsampled) as <cycle>.t<hh>z.<obtype>.tm00.bufr_d
    3. Runs for one cycle: 2018041512.
    
    closes #892 
    
    Overlapping windows application will be added in a subsequent PR
    
    ---------
    
    Co-authored-by: Guillaume Vernieres <guillaume.vernieres@noaa.gov>
    Co-authored-by: AndrewEichmann-NOAA <andrew.eichmann@noaa.gov>
    Co-authored-by: AndrewEichmann-NOAA <58948505+AndrewEichmann-NOAA@users.noreply.github.com>
    Co-authored-by: Cory Martin <cory.r.martin@noaa.gov>

--- test/soca/gw/setup_obsprep.sh
+++ test/soca/gw/setup_obsprep.sh
@@ -41,11 +41,19 @@ for PDY in "${PDYs[@]}"; do
                 done
             else
                 # Copy subsampled monthly in situ BUFR
-                # Example filename: tesac.201804.dcom_subsampled
+                # Example filename: tesac.201804.dcom_subsampled are copied as
+                # gdas.t00z.tesac.tm00.bufr_d (the original filename)
+                for typ in bathy dbuoy dbuoyb mbuoy mbuoyb ships shipsu tesac trkob; do
+                    if [ $(ls $testdatadir_bufr/${typ}.*subsampled 2>/dev/null | wc -l) -gt 0 ]; then
+                        cp -p $testdatadir_bufr/${typ}.*subsampled \
+                              $testdatadir_bufr/gdas.t${cyc}z.${typ}.tm00.bufr_d
+                    fi
+                done
+
                 # TODO: SP to replace these with daily and monthly subsampled with proper dates 
                 fullsubdir="$PDYdir/$cyc/atmos"
                 mkdir -p "$fullsubdir"
-                for file in "$testdatadir_bufr/*.${PDY:0:6}.dcom_subsampled"; do
+                for file in "$testdatadir_bufr/*.bufr_d"; do
                     cp -p $file $fullsubdir
                 done
             fi
