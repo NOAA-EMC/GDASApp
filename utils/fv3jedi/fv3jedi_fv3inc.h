1bda4af782dbe837b46ffcec4f08e08e1d8e86cc
DavidNew-NOAA <david.new@noaa.gov>
2024-04-02 11:00:26 -0500

    Modify fv3jedi_fv3inc.h to read ensembles

--- utils/fv3jedi/fv3jedi_fv3inc.h
+++ utils/fv3jedi/fv3jedi_fv3inc.h
@@ -14,6 +14,7 @@
 
 #include "oops/mpi/mpi.h"
 #include "oops/runs/Application.h"
+#include "oops/util/ConfigFunctions.h"
 #include "oops/util/DateTime.h"
 #include "oops/util/Logger.h"
 
@@ -27,56 +28,109 @@ namespace gdasapp {
     static const std::string classname() {return "gdasapp::fv3inc";}
 
     int execute(const eckit::Configuration & fullConfig, bool validate) const {
-      // Setup variable change
+
+      // Configurations
+      // ---------------------------------------------------------------------------------
+
+      // Variable change
       const eckit::LocalConfiguration varChangeConfig(fullConfig, "variable change");
       oops::Variables stateVarin(varChangeConfig, "input variables");
       oops::Variables stateVarout(varChangeConfig, "output variables");
 
-      // Setup background
-      const eckit::LocalConfiguration bkgConfig(fullConfig, "background");
-      const eckit::LocalConfiguration stateGeomConfig(bkgConfig, "geometry");
-      const eckit::LocalConfiguration stateInputConfig(bkgConfig, "input");
+      // Increment variables
+      oops::Variables jediIncrVarin(fullConfig, "increment variables");
+
+      // Geometries
+      const eckit::LocalConfiguration stateGeomConfig(fullConfig, "background geometry");
+      const eckit::LocalConfiguration jediIncrGeomConfig(fullConfig, "jedi increment geometry");
+      const eckit::LocalConfiguration fv3IncrGeomConfig(fullConfig, "fv3 increment geometry");
+
+      // Ensemble Members
+      int nmem;
+      std::vector<eckit::LocalConfiguration> membersConfig;    
+      if ( fullConfig.has("members") ) {
+	fullConfig.get("members", membersConfig);
+        nmem = membersConfig.size();
+      } else {
+	eckit::LocalConfiguration membersFromTemplateConfig(fullConfig, "members from template");
+	eckit::LocalConfiguration templateConfig(membersFromTemplateConfig, "template");
+	std::string pattern;
+        membersFromTemplateConfig.get("pattern", pattern);
+        membersFromTemplateConfig.get("nmembers", nmem);
+	int start = 1;
+	if (membersFromTemplateConfig.has("start")) {
+	  membersFromTemplateConfig.get("start", start);
+	}
+	std::vector<int> except;
+	if (membersFromTemplateConfig.has("except")) {
+	  membersFromTemplateConfig.get("except", except);
+	}
+        int zpad = 0;
+        if ( membersFromTemplateConfig.has("zero padding") ) {
+	  membersFromTemplateConfig.get("zero padding", zpad);
+	}
+
+        int count = start;
+        for ( int imem = 0; imem < nmem; imem++ ) {
+	  while (std::count(except.begin(), except.end(), count)) {
+	    count += 1;
+	  }
+	  eckit::LocalConfiguration memberConfig(templateConfig);
+	  util::seekAndReplace(memberConfig, pattern, count, zpad);  
+	  membersConfig.push_back(memberConfig);
+	  count += 1;
+	}
+      }
+     
+      // Setup
+      // ---------------------------------------------------------------------------------
+
+      // Geometries
       const fv3jedi::Geometry stateGeom(stateGeomConfig, this->getComm());
-      fv3jedi::State xxBkg(stateGeom, stateInputConfig);
-      oops::Log::test() << "Background State: " << std::endl << xxBkg << std::endl;
-
-      // Setup JEDI increment
-      const eckit::LocalConfiguration jediIncrConfig(fullConfig, "jedi increment");
-      const eckit::LocalConfiguration jediIncrGeomConfig(jediIncrConfig, "geometry");
-      const eckit::LocalConfiguration jediIncrInputConfig(jediIncrConfig, "input");
-      oops::Variables jediIncrVarin(jediIncrConfig, "input variables");
       const fv3jedi::Geometry jediIncrGeom(jediIncrGeomConfig, this->getComm());
-      fv3jedi::Increment dx(jediIncrGeom, jediIncrVarin, xxBkg.validTime());
-      dx.read(jediIncrInputConfig);
-      oops::Log::test() << "JEDI Increment: " << std::endl << dx << std::endl;
-
-      // Setup FV3 increment
-      const eckit::LocalConfiguration fv3IncrConfig(fullConfig, "fv3 increment");
-      const eckit::LocalConfiguration fv3IncrGeomConfig(fv3IncrConfig, "geometry");
-      const eckit::LocalConfiguration fv3IncrOuputConfig(fv3IncrConfig, "output");
       const fv3jedi::Geometry fv3IncrGeom(fv3IncrGeomConfig, this->getComm());
 
-      //
+      // Variable change                                                                                                               
       std::unique_ptr<fv3jedi::VariableChange> vc;
       vc.reset(new fv3jedi::VariableChange(varChangeConfig, stateGeom));
 
-      // ----------------------------------------------------------------------------
+      // Looping through ensemble member
+      // --------------------------------------------------------------------------------- 
 
-      // Add increment to background to get analysis
-      fv3jedi::State xxAnl(stateGeom, xxBkg);
-      xxAnl += dx;
+      for ( int imem = 0; imem < nmem; imem++ ) {
+        // Configurations
+	eckit::LocalConfiguration stateInputConfig(membersConfig[imem], "background input");
+	eckit::LocalConfiguration jediIncrInputConfig(membersConfig[imem], "jedi increment input");
+	eckit::LocalConfiguration fv3IncrOuputConfig(membersConfig[imem], "fv3 increment output");
 
-      // Perform variables change on background and analysis
-      vc->changeVar(xxBkg, stateVarout);
-      vc->changeVar(xxAnl, stateVarout);
+        // Setup background
+	fv3jedi::State xxBkg(stateGeom, stateInputConfig);
+        oops::Log::test() << "Background State: " << std::endl << xxBkg << std::endl;
 
-      // Get final FV3 increment
-      fv3jedi::Increment dxFV3(fv3IncrGeom, stateVarout, xxBkg.validTime());
-      dxFV3.diff(xxAnl, xxBkg);
-      oops::Log::test() << "FV3 Increment: " << std::endl << dxFV3 << std::endl;
+	// Setup JEDI increment
+	fv3jedi::Increment dx(jediIncrGeom, jediIncrVarin, xxBkg.validTime());
+	dx.read(jediIncrInputConfig);
+	oops::Log::test() << "JEDI Increment: " << std::endl << dx << std::endl;           
 
-      // Write FV3 increment
-      dxFV3.write(fv3IncrOuputConfig);
+        // Increment conversion
+	// ----------------------------------------------------------------------------
+
+	// Add increment to background to get analysis
+	fv3jedi::State xxAnl(stateGeom, xxBkg);
+	xxAnl += dx;
+
+	// Perform variables change on background and analysis
+	vc->changeVar(xxBkg, stateVarout);
+	vc->changeVar(xxAnl, stateVarout);
+
+	// Get final FV3 increment
+	fv3jedi::Increment dxFV3(fv3IncrGeom, stateVarout, xxBkg.validTime());
+	dxFV3.diff(xxAnl, xxBkg);
+	oops::Log::test() << "FV3 Increment: " << std::endl << dxFV3 << std::endl;
+
+	// Write FV3 increment
+	dxFV3.write(fv3IncrOuputConfig);
+      }
 
       return 0;
     }
