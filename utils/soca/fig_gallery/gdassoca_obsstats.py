249e242e33a33feeb1c81bedd51198309f669de0
Guillaume Vernieres <guillaume.vernieres@noaa.gov>
2024-05-09 09:27:36 -0400

    Make the seaice DA actually do something ... (#1092)
    - fixes #1087

--- utils/soca/fig_gallery/gdassoca_obsstats.py
+++ utils/soca/fig_gallery/gdassoca_obsstats.py
@@ -49,10 +49,13 @@ class ObsStats:
 
         # Get unique experiments
         experiments = filtered_data['Exp'].unique()
-
+        experiments.sort()
+        print(experiments)
+        
         # Plot settings
         fig, axs = plt.subplots(3, 1, figsize=(10, 15), sharex=True)
-        fig.suptitle(f'{inst} {variable} statistics, {ocean} ocean', fontsize=16, fontweight='bold')
+        fig.suptitle(f'{inst} {variable} statistics, {ocean} ocean', fontsize=18, fontweight='bold')
+
 
         exp_counter = 0
         for exp in experiments:
@@ -65,7 +68,7 @@ class ObsStats:
             axs[0].xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d %H'))
             axs[0].xaxis.set_major_locator(mdates.DayLocator())
             axs[0].tick_params(labelbottom=False)
-            axs[0].set_ylabel('RMSE')
+            axs[0].set_ylabel('RMSE', fontsize=18, fontweight='bold')
             axs[0].legend()
             axs[0].grid(True)
 
@@ -74,16 +77,18 @@ class ObsStats:
             axs[1].xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d %H'))
             axs[1].xaxis.set_major_locator(mdates.DayLocator())
             axs[1].tick_params(labelbottom=False)
-            axs[1].set_ylabel('Bias')
+            axs[1].set_ylabel('Bias', fontsize=18, fontweight='bold')
             axs[1].grid(True)
 
             # Plot Count
             axs[2].plot(exp_data['date'], exp_data['Count'], marker='o', linestyle='-', color=colors[exp_counter], label=exp)
             axs[2].xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d %H'))
             axs[2].xaxis.set_major_locator(mdates.DayLocator())
-            axs[2].set_ylabel('Count')
+            axs[2].set_ylabel('Count', fontsize=18, fontweight='bold')
             axs[2].grid(True)
 
+            exp_counter += 1
+            
         # Improve layout and show plot
         plt.tight_layout(rect=[0, 0.03, 1, 0.95])
         plt.savefig(f'{dirout}/{inst}_{variable}_{ocean}.png')
@@ -111,5 +116,5 @@ if __name__ == "__main__":
     obsStats = ObsStats()
     obsStats.read_csv(flist)
     for var, ocean in product(['ombg_noqc', 'ombg_qc'],
-                              ['Atlantic', 'Pacific', 'Indian', 'Arctic', 'Southern']):
+                              ['Global', 'Atlantic', 'Pacific', 'Indian', 'Arctic', 'Southern']):
         obsStats.plot_timeseries(ocean, var, inst=inst, dirout=args.dirout)
