6dc6139b0410c26ade2aaf0e156cac4501dbed29
Guillaume Vernieres <guillaume.vernieres@gmail.com>
2024-05-17 12:19:55 +0000

    overwrite exp

--- test/gw-ci/create_exp.sh
+++ test/gw-ci/create_exp.sh
@@ -6,13 +6,8 @@ export RUNTESTS="$4"/${pslot}
 export ICSDIR_ROOT=/scratch1/NCEPDEV/global/glopara/data/ICSDIR
 export SLURM_ACCOUNT="da-cpu"
 
-echo "pslot: ${pslot}"
-echo "HOMEgfs: ${HOMEgfs}"
-echo "expyaml: ${expyaml}"
-
 # Source the gw environement
 ${HOMEgfs}/workflow/gw_setup.sh
 
 # Create the experiment
-#rm -r ${RUNTESTS}
-${HOMEgfs}/workflow/create_experiment.py --yaml ${expyaml}
+${HOMEgfs}/workflow/create_experiment.py --yaml ${expyaml} --overwrite
