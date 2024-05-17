e445f27bd7e727c63e7a11b588e868d8979435b6
Guillaume Vernieres <guillaume.vernieres@noaa.gov>
2024-05-17 09:14:03 -0400

    don't spec. icsdir

--- test/gw-ci/create_exp.sh
+++ test/gw-ci/create_exp.sh
@@ -3,7 +3,6 @@ expyaml="$1"
 export pslot="$2"
 HOMEgfs="$3"
 export RUNTESTS="$4"/${pslot}
-export ICSDIR_ROOT=/scratch1/NCEPDEV/global/glopara/data/ICSDIR
 export SLURM_ACCOUNT="da-cpu"
 
 # Source the gw environement
