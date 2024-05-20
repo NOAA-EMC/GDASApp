c1f5b15d280e0401e95d0abd30f94cfba3bc421f
Guillaume Vernieres <guillaume.vernieres@gmail.com>
2024-05-20 20:16:12 +0000

    wip

--- test/gw-ci/create_exp.sh
+++ test/gw-ci/create_exp.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-expyaml="$1"
-export pslot="$2"
+
 HOMEgfs="$3"
-export RUNTESTS="$4"/${pslot}
-export SLURM_ACCOUNT="da-cpu"
 
 # Get ICSDIR_ROOT
 source "${HOMEgfs}/ush/detect_machine.sh"
 source "${HOMEgfs}/ci/platforms/config.${MACHINE_ID}"
+
+# Arguments
+expyaml="$1"
+export pslot="$2"
+export RUNTESTS="$4"/${pslot}
+export SLURM_ACCOUNT="da-cpu"
 
 # Source the gw environement
 source ${HOMEgfs}/workflow/gw_setup.sh
