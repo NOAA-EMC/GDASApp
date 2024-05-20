ef25435356117a01704b6345e9301d5d7e9c2d70
Guillaume Vernieres <guillaume.vernieres@gmail.com>
2024-05-20 14:32:42 +0000

    I lied ... ICSDIR_ROOT needs to be defined

--- test/gw-ci/create_exp.sh
+++ test/gw-ci/create_exp.sh
@@ -5,8 +5,12 @@ HOMEgfs="$3"
 export RUNTESTS="$4"/${pslot}
 export SLURM_ACCOUNT="da-cpu"
 
+# Get ICSDIR_ROOT
+source "${HOMEgfs}/ush/detect_machine.sh"
+source "${HOMEgfs}/ci/platforms/config.${MACHINE_ID}"
+
 # Source the gw environement
-${HOMEgfs}/workflow/gw_setup.sh
+source ${HOMEgfs}/workflow/gw_setup.sh
 
 # Create the experiment
 ${HOMEgfs}/workflow/create_experiment.py --yaml ${expyaml} --overwrite
