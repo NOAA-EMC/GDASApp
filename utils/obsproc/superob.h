d39de445499fbba7176571ae9d6eeefbc8deab13
ypwang19 <yaping.wang@users.noreply.github.com>
2024-05-21 14:40:24 +0000

    simplify calculation of distance using available functions

--- utils/obsproc/superob.h
+++ utils/obsproc/superob.h
@@ -5,39 +5,14 @@
 #include <vector>
 
 #include "eckit/config/LocalConfiguration.h"
+#include "atlas/util/Earth.h"
+#include "atlas/util/Geometry.h"
+#include "atlas/util/Point.h"
+
+
 
 namespace gdasapp {
   namespace superobutils {
-    // Function to compute distance between two points
-    struct GeoPoint {
-        double latitude;
-        double longitude;
-    };
-    const double EARTH_RADIUS_KM = 6371.0;
-
-    // Function to convert degrees to radians
-    double toRadians(double degree) {
-        return degree * (M_PI / 180.0);
-    }
-
-    // Function to calculate the Haversine distance between two points
-    double haversineDistance(const GeoPoint& point1, const GeoPoint& point2) {
-        double lat1 = toRadians(point1.latitude);
-        double lon1 = toRadians(point1.longitude);
-        double lat2 = toRadians(point2.latitude);
-        double lon2 = toRadians(point2.longitude);
-
-        // Haversine formula
-        double dlat = lat2 - lat1;
-        double dlon = lon2 - lon1;
-        double a = std::sin(dlat / 2.0) * std::sin(dlat / 2.0) +
-                   std::cos(lat1) * std::cos(lat2) *
-                   std::sin(dlon / 2.0) * std::sin(dlon / 2.0);
-        double c = 2.0 * std::atan2(std::sqrt(a), std::sqrt(1.0 - a));
-        double distance = EARTH_RADIUS_KM * c;
-        return distance;
-    }
-
     // Function to perform subsampling/binning of gridded data with a given stride
     template <typename T>
     std::vector<std::vector<T>> subsample2D(const std::vector<std::vector<T>>& inputArray,
@@ -109,9 +84,11 @@ namespace gdasapp {
                 int row = i * stride + si;
                 int col = j * stride + sj;
                 if (row < numRows && col < numCols && mask[row][col] == 1) {
-                  GeoPoint point1 = {inputlat[row][col], inputlon[row][col]};
-                  GeoPoint point2 = {targetlat[i][j], targetlon[i][j]};
-                  double distance = haversineDistance(point1, point2);
+
+                  atlas::PointLonLat p0(inputlon[row][col],inputlat[row][col]);
+                  atlas::PointLonLat p1(targetlon[i][j],targetlat[i][j]);
+                  double distance = atlas::util::Earth::distance(p0, p1)/1000.0;
+
                   double distance_sq = distance * distance;
                   double cressmanRadius_sq = cressmanRadius * cressmanRadius;
                   double weight = (distance <= cressmanRadius) ? (cressmanRadius_sq - distance_sq )
