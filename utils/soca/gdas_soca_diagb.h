024d26d860bceb2434157d1993cd4f5045f7bb4f
Guillaume Vernieres <guillaume.vernieres@gmail.com>
2024-06-13 14:39:03 +0000

    switched functionspace

--- utils/soca/gdas_soca_diagb.h
+++ utils/soca/gdas_soca_diagb.h
@@ -300,12 +300,14 @@ namespace gdasapp {
       }
 
       // Update the layer thickness halo
-      xbFs["hocn"].haloExchange();
+      //xbFs["hocn"].haloExchange();
+      nodeColumns.haloExchange(xbFs["hocn"]);
 
       // Loop through variables
       for (auto & var : configD.socaVars.variables()) {
         // Update the halo
-        xbFs[var].haloExchange();
+        //xbFs[var].haloExchange();
+        nodeColumns.haloExchange(xbFs[var]);
 
         // Skip the layer thickness variable
         if (var == "hocn") {
@@ -361,7 +363,9 @@ namespace gdasapp {
           // Horizontal averaging
           for (int iter = 0; iter < configD.niterHoriz; ++iter) {
             // Update the halo points
-            bkgErrFs[var].haloExchange();
+            //bkgErrFs[var].haloExchange();
+            nodeColumns.haloExchange(bkgErrFs[var]);
+
             auto stdDevBkg = atlas::array::make_view<double, 2>(bkgErrFs[var]);
 
             // Loops through nodes and levels
