0c7b4d11835ab204323c4e3598776813f09506ce
RussTreadon-NOAA <26926959+RussTreadon-NOAA@users.noreply.github.com>
2024-06-17 10:44:37 -0400

    use role.jedipara for hera ci (#1173)
    One task in #1150 is to move automated CI to role accounts. This PR
    moves Hera CI to role.jedipara.
    
    Fixes #1172
    
    ---------
    
    Co-authored-by: Guillaume Vernieres <guillaume.vernieres@noaa.gov>

--- ci/hera.sh
+++ ci/hera.sh
@@ -1,8 +1,8 @@
-export GDAS_CI_ROOT=/scratch1/NCEPDEV/da/Cory.R.Martin/CI/GDASApp
+export GDAS_CI_ROOT=/scratch1/NCEPDEV/da/role.jedipara/CI/GDASApp
 export GDAS_CI_HOST='hera'
 export GDAS_MODULE_USE=$GDAS_CI_ROOT/repo/modulefiles
 export SLURM_ACCOUNT=da-cpu
 export SALLOC_ACCOUNT=$SLURM_ACCOUNT
 export SBATCH_ACCOUNT=$SLURM_ACCOUNT
 export SLURM_QOS=debug
-export PATH=$PATH:/scratch1/NCEPDEV/da/Cory.R.Martin/CI/gh/gh_2.46.0_linux_amd64/bin
+export PATH=$PATH:/home/role.jedipara/bin
