985f6eac207b93284d1e97e35eae095a10ad4b79
Mindo Choi <mindo.choi@noaa.gov>
2024-06-20 10:22:41 -0500

    pynorm again

--- ush/soca/soca_vrfy.py
+++ ush/soca/soca_vrfy.py
@@ -110,7 +110,7 @@ def plotHorizontalSlice(config):
 
     # Add colorbar for filled contours
     cbar = fig.colorbar(contourf_plot, ax=ax, shrink=0.75, orientation='horizontal')
-    cbar.set_label(label_colorbar) 
+    cbar.set_label(label_colorbar)
 
     # Add contour lines with specified linewidths
     contour_levels = np.linspace(bounds[0], bounds[1], 5)
