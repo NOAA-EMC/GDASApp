324662226fa1fd652d5abb14fae4f88524a3ac62
Guillaume Vernieres <guillaume.vernieres@noaa.gov>
2024-07-20 07:37:25 -0500

    rocoto 1.3.7

--- modulefiles/GDAS/hercules.intel.lua
+++ modulefiles/GDAS/hercules.intel.lua
@@ -61,7 +61,7 @@ load("py-pybind11/2.11.0")
 --load("crtm/v2.4_jedi")
 load("contrib/0.1")
 load("noaatools/3.1")
-load("rocoto/1.3.5")
+load("rocoto/1.3.7")
 
 load("hpc/1.2.0")
 unload("python/3.10.13")
