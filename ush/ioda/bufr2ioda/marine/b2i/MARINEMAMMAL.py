4d15eb9fb895bcbd5605230e77b7a60356d468bd
Ed Givelberg <givelberg@users.noreply.github.com>
2024-08-06 13:45:44 -0400

    Update MARINEMAMMAL.py

--- ush/ioda/bufr2ioda/marine/b2i/MARINEMAMMAL.py
+++ ush/ioda/bufr2ioda/marine/b2i/MARINEMAMMAL.py
@@ -6,16 +6,14 @@ from marinemammal_ioda_variables import MarinemammalIODAVariables
 from bufr2ioda_converter import Bufr2ioda_Converter
 
 
-
-
 if __name__ == '__main__':
 
     script_name, config_file, log_file, test_file = ParseArguments()
 
     platform_description = 'Profiles from Marine Mammals: temperature and salinity'
-    bufr2ioda_config = Bufr2iodaConfig( \
-        script_name, \
-        config_file, \
+    bufr2ioda_config = Bufr2iodaConfig(
+        script_name,
+        config_file,
         platform_description)
 
     ioda_vars = MarinemammalIODAVariables()
@@ -28,4 +26,3 @@ if __name__ == '__main__':
     if test_file:
         result = mammal.test(test_file)
         sys.exit(result)
-
