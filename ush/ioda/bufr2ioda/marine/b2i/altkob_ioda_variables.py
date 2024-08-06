72d787b0e14505bc40d93cb70696f61d6f54537a
Ed Givelberg <givelberg@users.noreply.github.com>
2024-08-06 14:17:32 -0400

    Update altkob_ioda_variables.py

--- ush/ioda/bufr2ioda/marine/b2i/altkob_ioda_variables.py
+++ ush/ioda/bufr2ioda/marine/b2i/altkob_ioda_variables.py
@@ -8,25 +8,19 @@ class AltkobIODAVariables(IODAVariables):
     def __init__(self):
         super().__init__()
 
-
     def BuildQuery(self):
         q = super().BuildQuery()
-
         q.add('latitude', '*/CLATH')
         q.add('longitude', '*/CLONH')
-
         # ObsValue
         q.add('temp', '*/SST0')
         q.add('saln', '*/SSS0')
-
         return q
-
 
     def SetFromQueryResult(self, r):
         self.SetDatesFromQueryResult(r)
         self.SetLonLatFromQueryResult(r)
         self.SetObsFromQueryResult(r)
-
 
     def filter(self):
         mask = self.TemperatureFilter() \
@@ -43,9 +37,9 @@ class AltkobIODAVariables(IODAVariables):
 
     def SetAdditionalData(self):
         self.PreQC = (np.ma.masked_array(np.full((self.n_obs), 0))).astype(np.int32)
-        self.ObsError_temp = \ 
+        self.ObsError_temp = \
             np.float32(np.ma.masked_array(np.full((self.n_obs), self.ErrorT)))
-        self.ObsError_saln = \ 
+        self.ObsError_saln = \
             np.float32(np.ma.masked_array(np.full((self.n_obs), self.ErrorS)))
 
 
@@ -63,4 +57,3 @@ class AltkobIODAVariables(IODAVariables):
 
         self.WriteObsValueT(obsspace,'waterTemperature')
         self.WriteObsValueS(obsspace, 'salinity')
-
