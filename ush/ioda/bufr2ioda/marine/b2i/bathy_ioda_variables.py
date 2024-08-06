9ec439fe8eb722cdbc53176e2e7bb21b6c2379c7
Ed Givelberg <givelberg@users.noreply.github.com>
2024-08-06 14:30:47 -0400

    Update bathy_ioda_variables.py

--- ush/ioda/bufr2ioda/marine/b2i/bathy_ioda_variables.py
+++ ush/ioda/bufr2ioda/marine/b2i/bathy_ioda_variables.py
@@ -8,25 +8,19 @@ class BathyIODAVariables(IODAVariables):
     def __init__(self):
         super().__init__()
 
-
     def BuildQuery(self):
         q = super().BuildQuery()
-
         q.add('stationID', '*/RPID')
         q.add('latitude', '*/CLAT')
         q.add('longitude', '*/CLON')
         q.add('depth', '*/BTOCN/DBSS')
-
         # ObsValue
         q.add('temp', '*/BTOCN/STMP')
-
         return q
-
 
     def SetObsFromQueryResult(self, r):
         self.temp = r.get('temp', group_by='depth')
         self.temp -= 273.15
-
 
     def filter(self):
         mask = self.TemperatureFilter()
@@ -40,15 +34,12 @@ class BathyIODAVariables(IODAVariables):
         self.rcptdateTime = self.rcptdateTime[mask]
         self.temp = self.temp[mask]
 
-
     def SetAdditionalData(self):
         self.seqNum = Compute_sequenceNumber(self.lon)
         self.PreQC = (np.ma.masked_array(np.full((self.n_obs), 0))).astype(np.int32)
 
         # ObsError -- UNUSED
         # ObsError_temp = np.float32(np.ma.masked_array(np.full((len(temp)), 0.24)))
-
-
 
     def createIODAVars(self, obsspace):
         super().createIODAVars(obsspace)
@@ -60,11 +51,9 @@ class BathyIODAVariables(IODAVariables):
         self.WritePreQC(obsspace, "waterTemperature")
         self.WriteObsValueT(obsspace, "waterTemperature")
 
-
     def logObs(self, logger):
         self.logTemperature(logger)
 
     def logAdditionalData(self, logger):
         self.LogSeqNum(logger)
         self.LogPreQC(logger)
-
