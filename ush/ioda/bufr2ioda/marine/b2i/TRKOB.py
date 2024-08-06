a995f15775309826068bdd01307330a9f0c5b868
Ed Givelberg <givelberg@users.noreply.github.com>
2024-08-06 13:47:58 -0400

    Update TRKOB.py

--- ush/ioda/bufr2ioda/marine/b2i/TRKOB.py
+++ ush/ioda/bufr2ioda/marine/b2i/TRKOB.py
@@ -12,16 +12,14 @@ class TrkobConfig(Bufr2iodaConfig):
         return f"{self.cycle_type}.t{self.hh}z.insitu_surface_{self.data_format}.{self.cycle_datetime}.nc4"
 
 
-
-
 if __name__ == '__main__':
 
     script_name, config_file, log_file, test_file = ParseArguments()
 
     platform_description = 'Surface obs from TRACKOB: temperature and salinity'
-    bufr2ioda_config = TrkobConfig( \
-        script_name, \
-        config_file, \
+    bufr2ioda_config = TrkobConfig(
+        script_name,
+        config_file,
         platform_description)
 
     ioda_vars = TrkobIODAVariables()
@@ -34,4 +32,3 @@ if __name__ == '__main__':
     if test_file:
         result = trkob.test(test_file)
         sys.exit(result)
-
