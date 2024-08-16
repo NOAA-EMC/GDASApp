1580aa8aaff8500e3f4166ef462c70abff4cf8fc
CoryMartin-NOAA <cory.r.martin@noaa.gov>
2024-08-16 19:43:25 +0000

    update eva on orion

--- modulefiles/EVA/orion.lua
+++ modulefiles/EVA/orion.lua
@@ -6,11 +6,29 @@ local pkgName    = myModuleName()
 local pkgVersion = myModuleVersion()
 local pkgNameVer = myModuleFullName()
 
-prepend_path("MODULEPATH", '/work2/noaa/da/python/opt/modulefiles/stack')
+prepend_path("MODULEPATH", '/work/noaa/epic/role-epic/spack-stack/hercules/spack-stack-1.6.0/envs/unified-env/install/modulefiles/Core')
 
-load("hpc/1.2.0")
-load("miniconda3/4.6.14")
-load("eva/1.0.0")
+load("stack-intel/2021.9.0")
+load("stack-python/3.10.13")
+load("proj/9.2.1")
+load("py-matplotlib/3.7.3")
+load("py-xarray/2023.7.0")
+load("py-cartopy/0.21.1")
+load("py-scipy/1.11.3")
+
+local pyenvpath = "/work2/noaa/da/python/envs/"
+local pyenvname = "eva"
+
+local pyenvactivate = pathJoin(pyenvpath, pyenvname, "bin/activate")
+if (mode() == "load") then
+  local activate_cmd = "source "..pyenvactivate
+  execute{cmd=activate_cmd, modeA={"load"}}
+else
+  if (mode() == "unload") then
+    local deactivate_cmd = "deactivate"
+    execute{cmd=deactivate_cmd, modeA={"unload"}}
+  end
+end
 
 whatis("Name: ".. pkgName)
 whatis("Version: " .. pkgVersion)
