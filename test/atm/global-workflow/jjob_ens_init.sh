032b708f6a476ae4726d1533b82feb21fc8daa92
RussTreadon-NOAA <26926959+RussTreadon-NOAA@users.noreply.github.com>
2024-09-11 14:17:59 -0400

    Update atmospheric var and lgetkf ctests to use radiance bias correction tarball (#1267)

--- test/atm/global-workflow/jjob_ens_init.sh
+++ test/atm/global-workflow/jjob_ens_init.sh
@@ -73,16 +73,12 @@ for file in $flist; do
    ln -fs $GDASAPP_TESTDATA/lowres/$dpath/${oprefix}.${file}.nc4 $COM_OBS/${oprefix}.${file}.nc
 done
 
-# Link radiance bias correction files
+# Link radiance bias correction tarball
 dpath=gdas.$gPDY/$gcyc/analysis/atmos
 mkdir -p $COM_ATMOS_ANALYSIS_PREV
-flist="amsua_n19.satbias amsua_n19.satbias_cov"
+flist="rad_varbc_params.tar"
 for file in $flist; do
-   ln -fs $GDASAPP_TESTDATA/lowres/$dpath/$gprefix.${file}.nc4 $COM_ATMOS_ANALYSIS_PREV/$gprefix.${file}.nc
-done
-flist="amsua_n19.tlapse.txt"
-for file in $flist; do
-   ln -fs $GDASAPP_TESTDATA/lowres/$dpath/$gprefix.$file $COM_ATMOS_ANALYSIS_PREV/$gprefix.$file
+   ln -fs $GDASAPP_TESTDATA/lowres/$dpath/$gprefix.${file} $COM_ATMOS_ANALYSIS_PREV/$gprefix.${file}
 done
 
 # Link member atmospheric background on tiles and atmf006
