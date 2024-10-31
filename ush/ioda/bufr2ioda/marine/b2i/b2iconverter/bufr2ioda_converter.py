3a4112f2202fc46ed4bb2d974e788000500da11c
Edward Givelberg <thesqueezeison@yahoo.com>
2024-10-31 17:07:07 +0000

    exit 0; fixed python environment

--- ush/ioda/bufr2ioda/marine/b2i/b2iconverter/bufr2ioda_converter.py
+++ ush/ioda/bufr2ioda/marine/b2i/b2iconverter/bufr2ioda_converter.py
@@ -68,7 +68,7 @@ class Bufr2ioda_Converter:
         self.logger.debug(f"Query result has {n_obs} obs")
         if (n_obs == 0):
             self.logger.warning(f"No obs! Quitting.")
-            sys.exit(1)
+            sys.exit(0)
 
         self.ioda_vars.filter()
 
@@ -76,7 +76,7 @@ class Bufr2ioda_Converter:
         self.logger.debug(f"Filtered result has {n_obs} obs")
         if (n_obs == 0):
             self.logger.warning(f"No obs! Quitting.")
-            sys.exit(1)
+            sys.exit(0)
         self.logger.debug(f"Number of temperature obs = {self.ioda_vars.number_of_temp_obs()}")
         self.logger.debug(f"Number of salinity obs = {self.ioda_vars.number_of_saln_obs()}")
 
