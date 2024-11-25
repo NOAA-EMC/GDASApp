b1ae4d291dbc3984c46ead8625220e77223c4786
DavidNew-NOAA <david.new@noaa.gov>
2024-11-25 15:58:10 +0000

    Redo setting on ntasks for tests

--- ci/driver.sh
+++ ci/driver.sh
@@ -42,7 +42,6 @@ case ${TARGET} in
     module use $GDAS_MODULE_USE
     module load GDAS/$TARGET
     module list
-    export NTASKS_TESTS=12
     ;;
   *)
     echo "Unsupported platform. Exiting with error."
