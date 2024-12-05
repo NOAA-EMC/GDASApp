09eecd16373895bbc13186879087259a2020da7d
DavidNew-NOAA <david.new@noaa.gov>
2024-12-05 22:15:41 +0000

    Add hercules option

--- ci/driver.sh
+++ ci/driver.sh
@@ -41,7 +41,7 @@ done
 echo "Running automated testing on $TARGET"
 
 case ${TARGET} in
-  hera | orion)
+  hera | orion | hercules)
     source $MODULESHOME/init/sh
     source $my_dir/${TARGET}.sh
     module purge
