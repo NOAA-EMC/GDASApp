5d195d2fcf6951ab578a7dafa11b82dcbb0e5a40
DavidNew-NOAA <david.new@noaa.gov>
2025-01-31 01:20:44 +0000

    Coding norms

--- utils/fv3jedi/fv3jedi_ecen.h
+++ utils/fv3jedi/fv3jedi_ecen.h
@@ -7,7 +7,9 @@
 
 #include "eckit/config/LocalConfiguration.h"
 
-#include "fv3jedi/Utilities/Traits.h"
+#include "fv3jedi/Geometry/Geometry.h"
+#include "fv3jedi/Increment/Increment.h"
+#include "fv3jedi/State/State.h"
 
 #include "oops/mpi/mpi.h"
 #include "oops/runs/Application.h"
@@ -15,7 +17,6 @@
 #include "oops/util/DateTime.h"
 #include "oops/util/Duration.h"
 #include "oops/util/Logger.h"
-#include "oops/base/StructuredGridWriter.h"
 
 namespace gdasapp {
 
@@ -38,14 +39,16 @@ namespace gdasapp {
       // Get geometry configurations
       const eckit::LocalConfiguration incrGeomConfig(fullConfig, "increment geometry");
       const eckit::LocalConfiguration bkgGeomConfig(fullConfig, "background geometry");
-      const eckit::LocalConfiguration anlEnsMeanGeomConfig(fullConfig, "ensemble mean analysis geometry");
-      const eckit::LocalConfiguration incrCorGeomConfig(fullConfig, "correction increment geometry");
+      const eckit::LocalConfiguration anlEnsMeanGeomConfig(fullConfig, \
+                                                           "ensemble mean analysis geometry");
+      const eckit::LocalConfiguration incrCorGeomConfig(fullConfig, \
+                                                        "correction increment geometry");
 
       // Setup geometries
-      const oops::Geometry<fv3jedi::Traits> incrGeom(incrGeomConfig, this->getComm());
-      const oops::Geometry<fv3jedi::Traits> bkgGeom(bkgGeomConfig, this->getComm());
-      const oops::Geometry<fv3jedi::Traits> anlEnsMeanGeom(anlEnsMeanGeomConfig, this->getComm());
-      const oops::Geometry<fv3jedi::Traits> incrCorGeom(incrCorGeomConfig, this->getComm());
+      const fv3jedi::Geometry incrGeom(incrGeomConfig, this->getComm());
+      const fv3jedi::Geometry bkgGeom(bkgGeomConfig, this->getComm());
+      const fv3jedi::Geometry anlEnsMeanGeom(anlEnsMeanGeomConfig, this->getComm());
+      const fv3jedi::Geometry incrCorGeom(incrCorGeomConfig, this->getComm());
 
       // Get additions configuration
       int nhrs = fcstHours.size();
@@ -71,30 +74,32 @@ namespace gdasapp {
         // Get elements of individual additions configurations
         const eckit::LocalConfiguration atmIncrConfig(additionsConfig[ihrs], "atmospheric increment");
         const eckit::LocalConfiguration atmBkgConfig(additionsConfig[ihrs], "atmospheric background");
-        const eckit::LocalConfiguration atmAnlEnsMeanConfig(additionsConfig[ihrs], "atmospheric ensemble mean analysis");
-        const eckit::LocalConfiguration atmIncrCorConfig(additionsConfig[ihrs], "atmospheric correction increment");
+        const eckit::LocalConfiguration atmAnlEnsMeanConfig(additionsConfig[ihrs], \
+                                                            "atmospheric ensemble mean analysis");
+        const eckit::LocalConfiguration atmIncrCorConfig(additionsConfig[ihrs], \
+                                                         "atmospheric correction increment");
 
         // Initialize increment
-        oops::Increment<fv3jedi::Traits> dxAtm(incrGeom, atmVars, currentCycle);
+        fv3jedi::Increment dxAtm(incrGeom, atmVars, currentCycle);
         dxAtm.read(atmIncrConfig);
 
         // Initialize background state
-        oops::State<fv3jedi::Traits> xxAtmBkg(bkgGeom, atmVars, currentCycle);
+        fv3jedi::State xxAtmBkg(bkgGeom, atmVars, currentCycle);
         xxAtmBkg.read(atmBkgConfig);
 
         // Initialize ensemble mean analysis
-        oops::State<fv3jedi::Traits> xxAtmAnlEnsMean(anlEnsMeanGeom, atmVars, currentCycle);
+        fv3jedi::State xxAtmAnlEnsMean(anlEnsMeanGeom, atmVars, currentCycle);
         xxAtmAnlEnsMean.read(atmAnlEnsMeanConfig);
 
         // Compute analysis
-        oops::State<fv3jedi::Traits> xxAtmAnl(bkgGeom, xxAtmBkg);
+        fv3jedi::State xxAtmAnl(bkgGeom, xxAtmBkg);
         xxAtmAnl += dxAtm;
 
         // Interpolate full resolution analysis to ensemble resolution and then change variables
-        oops::State<fv3jedi::Traits> xxAtmAnlEnsRes(incrCorGeom, oops::State<fv3jedi::Traits>(atmVars, xxAtmAnl));
+        fv3jedi::State xxAtmAnlEnsRes(incrCorGeom, fv3jedi::State(atmVars, xxAtmAnl));
 
         // Compute correction increment
-        oops::Increment<fv3jedi::Traits> dxAtmCor(incrCorGeom, atmVars, xxAtmBkg.validTime());
+        fv3jedi::Increment dxAtmCor(incrCorGeom, atmVars, xxAtmBkg.validTime());
         dxAtmCor.diff(xxAtmAnlEnsMean, xxAtmAnlEnsRes);
 
         // Write correction increment
