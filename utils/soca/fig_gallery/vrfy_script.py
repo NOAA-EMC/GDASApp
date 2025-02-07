307d49e9e825fcc11c8f408240687768edd86fae
john.steffen <john.steffen@noaa.gov>
2025-02-07 15:24:48 +0000

    Fixes a typo and comments out portion of code that calls missing postproc netcdf files for plotting

--- utils/soca/fig_gallery/vrfy_script.py
+++ utils/soca/fig_gallery/vrfy_script.py
@@ -55,7 +55,7 @@ if plot_analysis:
                           colormap='jet',
                           projs=['North', 'South', 'Global'],
                           comout=os.path.join(comout, 'vrfy', 'ana'))]   # sea ice analysis
-    configs.extend(config_ana)
+    configs.extend(configs_ana)
 
 # Ensemble B plotting configuration
 if plot_ensemble_b:
@@ -176,16 +176,16 @@ if plot_increment:
                                                'hs_h': [-0.1, 0.1]},
                               colormap='seismic',
                               projs=['North', 'South'],
-                              comout=os.path.join(comout, 'vrfy', 'incr')),   # sea ice increment
-                   plotConfig(grid_file=grid_file,
-                              data_file=os.path.join(comout, f'{RUN}.t'+cyc+'z.ice.incr.postproc.nc'),
-                              lats=np.arange(-60, 60, 10),
-                              variables_horiz={'aice_h': [-0.2, 0.2],
-                                               'hi_h': [-0.5, 0.5],
-                                               'hs_h': [-0.1, 0.1]},
-                              colormap='seismic',
-                              projs=['North', 'South'],
-                              comout=os.path.join(comout, 'vrfy', 'incr.postproc'))]   # sea ice increment after postprocessing
+                              comout=os.path.join(comout, 'vrfy', 'incr'))]   # sea ice increment
+#                   plotConfig(grid_file=grid_file,
+#                              data_file=os.path.join(comout, f'{RUN}.t'+cyc+'z.ice.incr.postproc.nc'),
+#                              lats=np.arange(-60, 60, 10),
+#                              variables_horiz={'aice_h': [-0.2, 0.2],
+#                                               'hi_h': [-0.5, 0.5],
+#                                               'hs_h': [-0.1, 0.1]},
+#                              colormap='seismic',
+#                              projs=['North', 'South'],
+#                              comout=os.path.join(comout, 'vrfy', 'incr.postproc'))]   # sea ice increment after postprocessing
     configs.extend(config_incr)
 
 
