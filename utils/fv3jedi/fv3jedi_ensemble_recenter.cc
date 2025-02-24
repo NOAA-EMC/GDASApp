1cc2c4bb1550fa75e7d43aec7a207a10b4655401
DavidNew-NOAA <david.new@noaa.gov>
2025-02-24 20:54:29 +0000

    Update

--- utils/fv3jedi/fv3jedi_ensemble_recenter.cc
+++ utils/fv3jedi/fv3jedi_ensemble_recenter.cc
@@ -1,11 +1,12 @@
+#include "fv3jedi_add_increments.h"
+
 #include "fv3jedi/Utilities/Traits.h"
 
-#include "oops/runs/AddIncrement.h"
 #include "oops/runs/EnsembleApplication.h"
 #include "oops/runs/Run.h"
 
 int main(int argc,  char ** argv) {
   oops::Run run(argc, argv);
-  oops::EnsembleApplication<oops::AddIncrement<fv3jedi::Traits>> addIncrement;
+  oops::EnsembleApplication<gdasapp::AddIncrements> addIncrement;
   return run.execute(addIncrement);
 }
