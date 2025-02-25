31f41af67fe9475dce94ee1c29fb78995e18b270
DavidNew-NOAA <david.new@noaa.gov>
2025-02-25 14:49:22 +0000

    Remove old bespoke add increment app and add ensemble add increment app

--- utils/fv3jedi/fv3jedi_add_increments.cc
+++
@@ -1,8 +0,0 @@
-#include "fv3jedi_add_increments.h"
-#include "oops/runs/Run.h"
-
-int main(int argc,  char ** argv) {
-  oops::Run run(argc, argv);
-  gdasapp::AddIncrements AddIncrements;
-  return run.execute(AddIncrements);
-}
