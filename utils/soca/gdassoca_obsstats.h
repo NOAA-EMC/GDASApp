17f93f4607343eb838a6ed00f64a57bf119aee0e
DavidNew-NOAA <david.new@noaa.gov>
2025-04-17 15:16:13 +0000

    Update

--- utils/soca/gdassoca_obsstats.h
+++ utils/soca/gdassoca_obsstats.h
@@ -51,7 +51,7 @@ namespace gdasapp {
 
       // get the list of obs spaces to process
       std::vector<eckit::LocalConfiguration> obsSpaces;
-      fullConfig.get("observers", obsSpaces);
+      fullConfig.get("obs stats", obsSpaces);
 
       // get ensemble size if available
       size_t nens = fullConfig.getInt("nens", 0);
