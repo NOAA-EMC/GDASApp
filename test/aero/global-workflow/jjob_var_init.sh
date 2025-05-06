f6a27f256ded80a8a5f9cce7b3ba96843e461030
DavidNew-NOAA <david.new@noaa.gov>
2025-05-06 12:06:02 +0000

    COMIN/COMOUT for aero jjob tests

--- test/aero/global-workflow/jjob_var_init.sh
+++ test/aero/global-workflow/jjob_var_init.sh
@@ -62,10 +62,9 @@ oprefix=$CDUMP.t${cyc}z
 # Generate COM variables from templates
 YMD=${PDY} HH=${cyc} declare_from_tmpl -rx \
     COMIN_OBS:COM_OBS_TMPL
+
 RUN=${GDUMP} YMD=${gPDY} HH=${gcyc} declare_from_tmpl -rx \
-    COM_CHEM_ANALYSIS_PREV:COM_CHEM_ANALYSIS_TMPL \
-    COM_CHEM_HISTORY_PREV:COM_CHEM_HISTORY_TMPL \
-    COM_ATMOS_RESTART_PREV:COM_ATMOS_RESTART_TMPL
+    COMIN_ATMOS_RESTART_PREV:COM_ATMOS_RESTART_TMPL
 
 # Link observations
 dpath=gdas.$PDY/$cyc/obs
@@ -78,14 +77,14 @@ done
 
 # Copy model bacgkround on tiles
 dpath=gdas.$gPDY/$gcyc/model/atmos
-COM_ATMOS_RESTART_PREV_DIRNAME=$(dirname $COM_ATMOS_RESTART_PREV)
-if [ -d $COM_ATMOS_RESTART_PREV_DIRNAME/restart ]; then
-    rm -rf $COM_ATMOS_RESTART_PREV_DIRNAME/restart
+COMIN_ATMOS_RESTART_PREV_DIRNAME=$(dirname $COMIN_ATMOS_RESTART_PREV)
+if [ -d $COMIN_ATMOS_RESTART_PREV_DIRNAME/restart ]; then
+    rm -rf $COMIN_ATMOS_RESTART_PREV_DIRNAME/restart
 fi
-mkdir -p $COM_ATMOS_RESTART_PREV_DIRNAME/restart
+mkdir -p $COMIN_ATMOS_RESTART_PREV_DIRNAME/restart
 flist="restart/*"
 for file in $flist; do
-   cp $GDASAPP_TESTDATA/lowres/$dpath/$file $COM_ATMOS_RESTART_PREV_DIRNAME/restart/
+   cp $GDASAPP_TESTDATA/lowres/$dpath/$file $COMIN_ATMOS_RESTART_PREV_DIRNAME/restart/
 done
 
 
