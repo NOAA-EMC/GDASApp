f40a814ea212f3827ef63e9a7b9b75201bc3ecce
DavidNew-NOAA <david.new@noaa.gov>
2025-05-09 13:26:16 +0000

    Trying something

--- build.sh
+++ build.sh
@@ -110,6 +110,8 @@ CMAKE_OPTS+=" -DWORKFLOW_TESTS=${WORKFLOW_TESTS:-${WORKFLOW_BUILD}}"
 
 # Link MOM6 and Icepack in SOCA to submodules in the UFS repo
 if [[ $WORKFLOW_BUILD == 'ON' ]]; then
+  rm -rf $dir_root/sorc/soca/external/mom6/MOM6
+  rm -rf $dir_root/sorc/soca/external/icepack/Icepack
   ln -sf $dir_root/../ufs_model.fd/MOM6-interface/MOM6/ $dir_root/sorc/soca/external/mom6/MOM6
   ln -sf $dir_root/../ufs_model.fd/CICE-interface/CICE/icepack/ $dir_root/sorc/soca/external/icepack/Icepack
 fi
