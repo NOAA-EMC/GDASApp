33f9994932bb37856447c7d9440d2c51b5108b4d
Guillaume Vernieres <guillaume.vernieres@noaa.gov>
2025-05-16 14:48:39 -0400

    remove old diagb files

--- utils/soca/gdas_soca_diagb.cc
+++
@@ -1,8 +0,0 @@
-#include "gdas_soca_diagb.h"
-#include "oops/runs/Run.h"
-
-int main(int argc,  char ** argv) {
-  oops::Run run(argc, argv);
-  gdasapp::SocaDiagB diagb;
-  return run.execute(diagb);
-}
