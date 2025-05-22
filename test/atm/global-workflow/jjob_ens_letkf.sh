dd5758eb75b5e193f5f8c77e9c7f71eb1dfcdf06
RussTreadon-NOAA <Russ.Treadon@noaa.gov>
2025-05-22 16:20:39 +0000

    reduce jjob_ens_letkf memory request to 32 Gb (#1695)

--- test/atm/global-workflow/jjob_ens_letkf.sh
+++ test/atm/global-workflow/jjob_ens_letkf.sh
@@ -55,7 +55,7 @@ cp $EXPDIR/config.base_lobsdiag_forenkf_false $EXPDIR/config.base
 if [[ $machine = 'HERA' || $machine = 'ORION' || $machine = 'HERCULES' ]]; then
     sbatch --nodes=1 --ntasks=6 --account=$ACCOUNT --qos=batch --time=00:30:00 --export=ALL --wait --output=atmensanlletkf-%j.out ${HOMEgfs}/jobs/JGLOBAL_ATMENS_ANALYSIS_LETKF
 elif [[ $machine = 'URSA' ]]; then
-     sbatch --nodes=1 --ntasks=6 --account=$ACCOUNT --qos=batch --partition=u1-compute --mem="96Gb" --time=00:30:00 --export=ALL --wait --output=atmensanlletkf-%j.out ${HOMEgfs}/jobs/JGLOBAL_ATMENS_ANALYSIS_LETKF
+     sbatch --nodes=1 --ntasks=6 --account=$ACCOUNT --qos=batch --partition=u1-compute --mem="32Gb" --time=00:30:00 --export=ALL --wait --output=atmensanlletkf-%j.out ${HOMEgfs}/jobs/JGLOBAL_ATMENS_ANALYSIS_LETKF
 else
     ${HOMEgfs}/jobs/JGLOBAL_ATMENS_ANALYSIS_LETKF
 fi
