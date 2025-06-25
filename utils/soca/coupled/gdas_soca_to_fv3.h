1590339bf1bca97f46b4a306afd623e235d16534
Guillaume Vernieres <guillaume.vernieres@noaa.gov>
2025-06-25 16:23:54 -0400

    read soca and fv3jedi states

--- utils/soca/coupled/gdas_soca_to_fv3.h
+++ utils/soca/coupled/gdas_soca_to_fv3.h
@@ -1,9 +1,5 @@
 # pragma once
 
-// No clue why but lint is complaining, so adding old style header guard
-#ifndef BUNDLE_GDAS_UTILS_SOCA_COUPLED_GDAS_SOCA_TO_FV3_H_
-#define BUNDLE_GDAS_UTILS_SOCA_COUPLED_GDAS_SOCA_TO_FV3_H_
-
 #include <string>
 
 #include "atlas/array.h"
@@ -11,12 +7,15 @@
 #include "eckit/config/LocalConfiguration.h"
 
 #include "fv3jedi/Geometry/Geometry.h"
+#include "fv3jedi/State/State.h"
 
 #include "oops/mpi/mpi.h"
 #include "oops/runs/Application.h"
+#include "oops/util/DateTime.h"
 #include "oops/util/Logger.h"
 
 #include "soca/Geometry/Geometry.h"
+#include "soca/State/State.h"
 
 namespace gdasapp {
 namespace coupled {
@@ -37,7 +36,7 @@ class SocaToFv3 : public oops::Application {
     oops::Log::info() << "socaGeom: " << socaGeom << std::endl;
     oops::Log::info() << "soca lonlat: " << std::endl;
     for (int jnode = 0; jnode < socalonlat.shape(0); ++jnode) {
-      oops::Log::info() << "  " << socalonlat(jnode, 0) << ", "
+      oops::Log::debug() << "  " << socalonlat(jnode, 0) << ", "
                         << socalonlat(jnode, 1) << std::endl;
     }
 
@@ -46,11 +45,26 @@ class SocaToFv3 : public oops::Application {
     const fv3jedi::Geometry fv3jediGeom(fv3Config, this->getComm());
     auto fv3lonlat = atlas::array::make_view<double, 2>(fv3jediGeom.functionSpace().lonlat());
     oops::Log::info() << "fv3jediGeom: " << fv3jediGeom << std::endl;
-    oops::Log::info() << "fv3 lonlat: " << std::endl;
+    oops::Log::debug() << "fv3 lonlat: " << std::endl;
     for (int jnode = 0; jnode < fv3lonlat.shape(0); ++jnode) {
-      oops::Log::info() << "  " << fv3lonlat(jnode, 0) << ", "
+      oops::Log::debug() << "  " << fv3lonlat(jnode, 0) << ", "
                         << fv3lonlat(jnode, 1) << std::endl;
     }
+
+    // Date
+    std::string strdt;
+    fullConfig.get("date", strdt);
+    auto stateDate = util::DateTime(strdt);
+
+    // Read fv3 and soca states
+    const eckit::LocalConfiguration fv3StateConfig(fullConfig, "fv3.state");
+    fv3jedi::State fv3State(fv3jediGeom, fv3StateConfig);
+    oops::Log::info() << "fv3State: " << fv3State << std::endl;
+
+    auto socaVars = oops::Variables(fullConfig, "soca.variables");
+    soca::State socaState(socaGeom, socaVars, stateDate);
+    socaState.read(eckit::LocalConfiguration(fullConfig, "soca.state"));
+    oops::Log::info() << "socaState" << socaState << std::endl;
 
     return 0;
   }
@@ -62,5 +76,3 @@ class SocaToFv3 : public oops::Application {
 };
 }  // namespace coupled
 }  // namespace gdasapp
-
-#endif  // BUNDLE_GDAS_UTILS_SOCA_COUPLED_GDAS_SOCA_TO_FV3_H_
