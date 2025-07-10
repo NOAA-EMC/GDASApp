7dba700c8e35f5ab93037c415a6e486f2df3fde3
Taylor Roper <taylor.roper@noaa.gov>
2025-07-10 19:21:41 +0000

    Update to spack-stack 1.9.2 (#1777)

--- modulefiles/GDAS/noaacloud.intel.lua
+++ modulefiles/GDAS/noaacloud.intel.lua
@@ -6,7 +6,7 @@ local pkgName    = myModuleName()
 local pkgVersion = myModuleVersion()
 local pkgNameVer = myModuleFullName()
 
-prepend_path("MODULEPATH", "/contrib/spack-stack-rocky8/spack-stack-1.9.1/envs/ue-oneapi-2024.2.1/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/contrib/spack-stack-rocky8/spack-stack-1.9.2/envs/ue-oneapi-2024.2.1/install/modulefiles/Core")
 
 load("gnu/13.2.0")
 load("stack-oneapi/2024.2.1")
