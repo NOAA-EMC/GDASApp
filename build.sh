b97cae03d12e1bf4ee135c5dfc8f7226e1cfe38e
Russ-Treadon-NOAA <Russ.Treadon@noaa.gov>
2025-07-22 13:45:42 +0000

    use git patch to update AtlasInterpolator.cc on wcoss2

--- build.sh
+++ build.sh
@@ -92,11 +92,12 @@ CMAKE_OPTS+=" -DCLONE_JCSDADATA=$CLONE_JCSDADATA -DMACHINE=$BUILD_TARGET"
 # TODO: Remove AtlasInterpolator_boost.cc when WCOSS2 c++ accepts std::inclusive_sum
 if [[ $BUILD_TARGET == 'wcoss2' ]]; then
     export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:/opt/cray/pe/mpich/8.1.19/ofi/intel/19.0/lib"
-    mv $dir_root/sorc/oops/src/oops/generic/AtlasInterpolator.cc $dir_root/sorc/oops/src/oops/generic/AtlasInterpolator_original.cc
-    cp $dir_root/AtlasInterpolator_boost.cc $dir_root/sorc/oops/src/oops/generic/AtlasInterpolator.cc
     echo ""
-    echo "***WARNING*** Replace oops AtlasInterpolator.cc with ${BUILD_TARGET} workaround ***WARNING***"
+    echo "***WARNING*** Apply boost patch to oops AtlasInterpolator.cc on ${BUILD_TARGET} ***WARNING***"
     echo ""
+    cd $dir_root/sorc/oops/src/oops/generic/
+    git apply $dir_root/AtlasInterpolator_boost_patch.txt
+    cd $dir_root
 fi
 
 BUILD_DIR=${BUILD_DIR:-$dir_root/build}
