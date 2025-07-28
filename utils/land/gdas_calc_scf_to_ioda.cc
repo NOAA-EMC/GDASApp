7506c46745ccf64106718723057998bb650d792f
CoryMartin-NOAA <cory.r.martin@noaa.gov>
2025-07-28 18:05:46 +0000

    fix compile issue, missing semicolon

--- utils/land/gdas_calc_scf_to_ioda.cc
+++ utils/land/gdas_calc_scf_to_ioda.cc
@@ -125,7 +125,7 @@ void gdasapp::CalcSCFtoIODA::calc_fcst_snow_density(fv3jedi::State & bkgState,
       bkg_density(jnode, 0) = bkg_swe(jnode, 0) / bkg_snd(jnode, 0);
     } else {
       // snow is not present, use average from snow forecasts over land
-      double tmp_density = 67.92 + 51.25 * std::exp((bkg_stc(jnode, 0)- 273.15) / 2.59)
+      double tmp_density = 67.92 + 51.25 * std::exp((bkg_stc(jnode, 0)- 273.15) / 2.59);
       bkg_density(jnode, 0) =
         std::max(80.0, std::min(120.0, tmp_density)) / 1000.0;
     }
