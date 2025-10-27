6f6c112618c01fb9255e730c52ef0abe131ae7cc
Cory Martin <Cory.R.Martin@noaa.gov>
2025-10-27 20:28:11 +0000

    update regression test script

--- test/atm/regression_test_3dvar.sh
+++ test/atm/regression_test_3dvar.sh
@@ -190,7 +190,7 @@ algorithm: 3dvar
 
 # Observation things
 # ------------------
-observations: all_observations
+observations: [prepbufr_adpsfc, scatwnd_ascat_metop-c, satwnd_amv_abi_goes-18]
 
 crtm_coefficient_path: "$rundir/crtm/"
 
