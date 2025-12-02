57a6c46c75f2d26b5caa5b5e0722f27a15549a98
RussTreadon-NOAA <russ.treadon@noaa.gov>
2025-12-02 16:25:43 +0000

    enable gcafs gw CI on MSU, add ufsgsi_hybatmDA to CI_tests

--- ci/driver.sh
+++ ci/driver.sh
@@ -194,12 +194,6 @@ for pr in $open_pr_list; do
       ci_checklist=$(echo "$branch_body" | grep -i '\[x\]')
       ctest_regex_exclude=""
 
-      ## TODO - remove logic below when GCAFS ctest run on MSU (Hercules and Orion)
-      if [[ "${TARGET}" = "hercules" || "${TARGET}" = "orion" ]]; then
-        ci_test="C96_gcafs_cycled"
-        ctest_regex_exclude+="${ctest_regex_exclude:+|}$ci_test"
-      fi
-      
       for ci_test in ${CI_TESTS[@]}; do
         if ! echo "$ci_checklist" | grep -q "$ci_test"; then
 	  ctest_regex_exclude+="${ctest_regex_exclude:+|}$ci_test"
