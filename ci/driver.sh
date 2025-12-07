a726ce7c9f95edd8d19d3695299805ec5b94d842
RussTreadon-NOAA <russ.treadon@noaa.gov>
2025-12-07 07:26:56 -0500

    add gaeac6 to ci and stable_driver

--- ci/driver.sh
+++ ci/driver.sh
@@ -49,6 +49,16 @@ case ${TARGET} in
     module load GDAS/$TARGET
     module list
     ;;
+  gaeac6)
+      source $my_dir/${TARGET}.sh
+      if ( ! eval module help > /dev/null 2>&1 ) ; then
+          source /etc/profile
+      fi
+      module reset
+      module use $GDAS_MODULE_USE
+      module load GDAS/$TARGET
+      module list
+    ;;
   *)
     echo "Unsupported platform. Exiting with error."
     exit 1
