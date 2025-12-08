5c0364f9dfba85c543f0818b441da7037386a48a
RussTreadon-NOAA <Russ.Treadon@noaa.gov>
2025-12-08 18:13:16 +0000

    update path to g-w jobs for test_gdasapp_atm_jjob

--- test/atm/global-workflow/jjob_ens_letkf.sh
+++ test/atm/global-workflow/jjob_ens_letkf.sh
@@ -62,7 +62,7 @@ nodes: 1
 ntasks_per_node: 6
 threads_per_task: 1
 memory: ${memory}
-command: ${HOMEgfs}/jobs/JGLOBAL_ATMENS_ANALYSIS_LETKF
+command: ${HOMEgfs}/dev/jobs/JGLOBAL_ATMENS_ANALYSIS_LETKF
 filename: submit_${type}.sh
 EOF
 
@@ -76,5 +76,6 @@ if [[ $SCHEDULER = 'slurm' ]]; then
 elif [[ $SCHEDULER = 'pbspro' ]]; then
     qsub -V -W block=true submit_${type}.sh
 else
-    ${HOMEgfs}/jobs/JGLOBAL_ATMENS_ANALYSIS_LETKF
+
+    ${HOMEgfs}/dev/jobs/JGLOBAL_ATMENS_ANALYSIS_LETKF
 fi
