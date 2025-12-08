1ae1ee008e7e1bd4d7192cfbc26e453585fff2f5
RussTreadon-NOAA <26926959+RussTreadon-NOAA@users.noreply.github.com>
2025-12-08 10:17:02 -0500

    Weekly update of JEDI hashes (20251208) (#1997)
    # Description
    
    This PR consists of two sets of changes
    - weekly update of select JEDI hashes
    - enable C96_gcafs_cycled CI on MSU
    - enable CI on Gaea C6

---
+++ ci/gaeac6.sh
@@ -0,0 +1,10 @@
+export GDAS_CI_ROOT=/gpfs/f6/ira-sti/scratch/Russ.Treadon/CI/GDASApp
+export GDAS_CI_HOST='gaeac6'
+export GDAS_MODULE_USE=$GDAS_CI_ROOT/repo/modulefiles
+export SLURM_ACCOUNT=gfs-cpu
+export SALLOC_ACCOUNT=$SLURM_ACCOUNT
+export SBATCH_ACCOUNT=$SLURM_ACCOUNT
+export SLURM_QOS=normal
+export PATH=$PATH:/ncrc/home1/Russ.Treadon
+export NTASKS_TESTS=12
+export AUTHORIZED_USERS_FILE=/gpfs/f6/ira-sti/scratch/Russ.Treadon/CI/GDASApp/authorized_users
