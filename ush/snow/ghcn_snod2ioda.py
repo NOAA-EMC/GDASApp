9123df84cd6d76fb705af4f6d841fdc896f7112e
jiaruidong2017 <jiarui.dong@noaa.gov>
2025-12-29 14:29:35 -0500

    Fix norm error

--- ush/snow/ghcn_snod2ioda.py
+++ ush/snow/ghcn_snod2ioda.py
@@ -3,7 +3,8 @@
 # (C) Copyright 2021-2022 NOAA/NWS/NCEP/EMC
 #
 # This software is licensed under the terms of the Apache Licence Version 2.0
-# This file is copied from the JEDI ioda-converts repo. 
+# This file is copied from the JEDI ioda-converts repo.
+#
 
 import argparse
 import numpy as np
