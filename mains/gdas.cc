9f2dabe43d42d4e65ddb63192297657d8e78b536
Cory Martin <Cory.R.Martin@noaa.gov>
2026-01-29 20:06:20 +0000

    make SOCA optional

--- mains/gdas.cc
+++ mains/gdas.cc
@@ -6,8 +6,10 @@
 #include "fv3jedi/ObsLocalization/instantiateObsLocFactory.h"
 #include "fv3jedi/Utilities/Traits.h"
 
+#ifdef BUILD_SOCA
 #include "soca/GeometryIterator/GeometryIterator.h"
 #include "soca/Traits.h"
+#endif
 
 #include "saber/oops/instantiateCovarFactory.h"
 #include "ufo/instantiateObsFilterFactory.h"
@@ -39,8 +41,10 @@ int runApp(int argc, char** argv, const std::string traits, const std::string ap
   if (appName == "localensembleda") {
     if (traits == "fv3jedi") {
       fv3jedi::instantiateObsLocFactory();
+#ifdef BUILD_SOCA
     } else if (traits == "soca") {
       ufo::instantiateObsLocFactory<soca::GeometryIterator>();
+#endif
     }
   }
 
@@ -97,7 +101,11 @@ int main(int argc,  char ** argv) {
 
   // Check that the traits are recognized
   // ------------------------------------
+#ifdef BUILD_SOCA
   const std::set<std::string> validTraits = {"fv3jedi", "soca"};
+#else
+  const std::set<std::string> validTraits = {"fv3jedi"};
+#endif
   ASSERT_MSG(validTraits.find(traits) != validTraits.end(), "Traits not recognized: " + traits);
 
   // Check that the application is recognized
@@ -124,8 +132,10 @@ int main(int argc,  char ** argv) {
   if (traits == "fv3jedi") {
     fv3jedi::instantiateObsLocFactory();
     return runApp<fv3jedi::Traits>(argc, argv, traits, app);
+#ifdef BUILD_SOCA
   } else if (traits == "soca") {
     return runApp<soca::Traits>(argc, argv, traits, app);
+#endif
   }
 }
 
