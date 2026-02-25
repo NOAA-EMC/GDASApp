2c3ce356fd1a0e7e99232692751c55d9c8fffc36
Wei Huang <Wei.Huang@noaa.gov>
2026-02-25 13:44:59 -0700

    add module for  derecho

--- build.sh
+++ build.sh
@@ -78,7 +78,7 @@ while getopts "w:t:c:hvdfai" opt; do
 done
 
 case ${BUILD_TARGET} in
-  hera | orion | hercules | wcoss2 | noaacloud | gaeac6 | ursa | container )
+  hera | orion | hercules | wcoss2 | noaacloud | gaeac6 | ursa | derecho | container )
     echo "Building GDASApp on $BUILD_TARGET"
     source $dir_root/ush/module-setup.sh
     module use $dir_root/modulefiles
@@ -138,12 +138,12 @@ CMAKE_OPTS+=" -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX}"
 CMAKE_OPTS+=" -DCMAKE_INSTALL_LIBDIR=${CMAKE_INSTALL_LIBDIR}"
 
 # JCSDA changed test data things, need to make a dummy CRTM directory
-if [ -d "$dir_root/bundle/fix/test-data-release/" ]; then rm -rf $dir_root/bundle/fix/test-data-release/; fi
-if [ -d "$dir_root/bundle/test-data-release/" ]; then rm -rf $dir_root/bundle/test-data-release/; fi
-mkdir -p $dir_root/bundle/fix/test-data-release/
-mkdir -p $dir_root/bundle/test-data-release/
-ln -sf $GDASAPP_TESTDATA/crtm $dir_root/bundle/fix/test-data-release/crtm
-ln -sf $GDASAPP_TESTDATA/crtm $dir_root/bundle/test-data-release/crtm
+# if [ -d "$dir_root/bundle/fix/test-data-release/" ]; then rm -rf $dir_root/bundle/fix/test-data-release/; fi
+# if [ -d "$dir_root/bundle/test-data-release/" ]; then rm -rf $dir_root/bundle/test-data-release/; fi
+# mkdir -p $dir_root/bundle/fix/test-data-release/
+# mkdir -p $dir_root/bundle/test-data-release/
+# ln -sf $GDASAPP_TESTDATA/crtm $dir_root/bundle/fix/test-data-release/crtm
+# ln -sf $GDASAPP_TESTDATA/crtm $dir_root/bundle/test-data-release/crtm
 
 # Configure
 echo "Configuring ... `date`"
