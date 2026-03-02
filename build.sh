a308444f64186e1d212694c6108786f184737160
Wei Huang <Wei.Huang@noaa.gov>
2026-03-02 10:43:12 -0700

    add a todo, not remove the crtm testpath

--- build.sh
+++ build.sh
@@ -137,6 +137,17 @@ CMAKE_OPTS+=" -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX}"
 # Set CMAKE_INSTALL_LIBDIR as CMake option
 CMAKE_OPTS+=" -DCMAKE_INSTALL_LIBDIR=${CMAKE_INSTALL_LIBDIR}"
 
+# TODO: allow CRTM paths to come from the modules on machines other than derecho, and container
+if [[ "${BUILD_TARGET}" != "derecho" && "${BUILD_TARGET}" != "container" ]]; then
+# JCSDA changed test data things, need to make a dummy CRTM directory
+if [ -d "$dir_root/bundle/fix/test-data-release/" ]; then rm -rf $dir_root/bundle/fix/test-data-release/; fi
+if [ -d "$dir_root/bundle/test-data-release/" ]; then rm -rf $dir_root/bundle/test-data-release/; fi
+mkdir -p $dir_root/bundle/fix/test-data-release/
+mkdir -p $dir_root/bundle/test-data-release/
+ln -sf $GDASAPP_TESTDATA/crtm $dir_root/bundle/fix/test-data-release/crtm
+ln -sf $GDASAPP_TESTDATA/crtm $dir_root/bundle/test-data-release/crtm
+fi
+
 # Configure
 echo "Configuring ... `date`"
 set -x
