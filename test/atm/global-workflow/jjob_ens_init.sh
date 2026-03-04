e4aa42902678b3ebda2e2d1b99d3caeef7523a45
RussTreadon-NOAA <russ.treadon@noaa.gov>
2026-03-04 13:40:44 -0500

    correct typo in name of varbc tarball for atm_jjob init jobs

--- test/atm/global-workflow/jjob_ens_init.sh
+++ test/atm/global-workflow/jjob_ens_init.sh
@@ -70,7 +70,7 @@ done
 # Link radiance bias correction tarball
 dpath=gdas.$gPDY/$gcyc/analysis/atmos
 mkdir -p $COMIN_ATMOS_ANALYSIS_PREV
-flist="rad_varbc_params.tar"
+flist="varbc_params.tar"
 for file in $flist; do
    ln -fs $GDASAPP_TESTDATA/lowres/$dpath/$gprefix.${file} $COMIN_ATMOS_ANALYSIS_PREV/$gprefix.${file}
 done
